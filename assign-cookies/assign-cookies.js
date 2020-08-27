79f8fdebfbe8a783d500e623bdfd0b5922e49538
MalindiJohn <malindi.wambua9789@gmail.com>
2020-08-27 12:35:33 +0300

    testing assign-cookies js logic

--- assign-cookies/assign-cookies.js
+++ assign-cookies/assign-cookies.js
@@ -28,3 +28,13 @@ var findContentChildren = function (g, s) {
 console.log(findContentChildren([1, 2, 3], [1, 1])); // 1
 console.log(findContentChildren([1, 2], [1, 2, 3])); // 2
 console.log(findContentChildren([1, 2, 3], [3])); // 1
+console.log(findContentChildren([1, 2, 3], [1, 2, 3])); // 3
+console.log(findContentChildren([1, 2, 3], [3, 2, 1])); // 3
+console.log(findContentChildren([1, 2, 3], [1, 2, 3, 4])); // 3
+console.log(findContentChildren([1, 2, 3, 4], [1, 2, 3])); // 3
+console.log(findContentChildren([1, 2, 3, 4], [1, 2, 3, 4])); // 4
+console.log(findContentChildren([1, 2, 3, 4], [4, 3, 2, 1])); // 4
+console.log(findContentChildren([1, 2, 3, 4], [4, 3, 2, 1, 5])); // 4
+console.log(findContentChildren([1, 2, 3, 4, 5], [4, 3, 2, 1])); // 4
+console.log(findContentChildren([1, 2, 3, 4, 5], [4, 3, 2, 1, 5])); // 5
+console.log(findContentChildren([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])); // 5
