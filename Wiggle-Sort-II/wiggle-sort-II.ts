3f0d8ff6a7442428f16942ab05f27288ef2056e2
MalindiJohn <malindi.wambua9789@gmail.com>
2022-01-05 10:00:33 +0300

    testing wiggleSort function logic

--- Wiggle-Sort-II/wiggle-sort-II.ts
+++ Wiggle-Sort-II/wiggle-sort-II.ts
@@ -19,12 +19,23 @@ function wiggleSort(nums: number[]): void {
 const nums1 = [1, 5, 1, 1, 6, 4];
 wiggleSort(nums1);
 console.log(nums1); // expected [1, 4, 1, 5, 1, 6]
+
 const nums2 = [1, 3, 2, 2, 3, 1];
 wiggleSort(nums2);
 console.log(nums2); // expected [2, 3, 1, 3, 1, 2]
+
 const nums3 = [1, 3, 2, 2, 3];
 wiggleSort(nums3);
 console.log(nums3); // expected [2, 3, 1, 3, 2]
+
 const nums4 = [1, 3, 2, 2, 3, 1, 3];
 wiggleSort(nums4);
 console.log(nums4); // expected [2, 3, 1, 3, 1, 3, 2]
+
+const nums5 = [1, 3, 2, 2, 3, 1, 3, 1];
+wiggleSort(nums5);
+console.log(nums5); // expected [1, 3, 1, 3, 1, 3, 2, 2]
+
+const nums6 = [1, 3, 2, 2, 3, 1, 3, 1, 3];
+wiggleSort(nums6);
+console.log(nums6); // expected [1, 3, 1, 3, 1, 3, 1, 3, 2]
