daf2636983c0f92c4b464546ad687e468167de9f
MalindiJohn <malindi.wambua9789@gmail.com>
2022-01-08 10:00:33 +0300

    added wiggleSort js function logic

--- Wiggle-Sort-II/wiggle-sort-II.js
+++ Wiggle-Sort-II/wiggle-sort-II.js
@@ -3,5 +3,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var wiggleSort = function(nums) {
+
+    let n = nums.length;
+    let mid = Math.floor((n + 1) / 2);
+    let sorted = nums.slice().sort((a, b) => a - b);
+
+    for (let i = 0, j = mid, k = n - 1; i < n; i++) {
+        nums[i] = i % 2 === 0 ? sorted[--j] : sorted[k--];
+    }
     
 };
