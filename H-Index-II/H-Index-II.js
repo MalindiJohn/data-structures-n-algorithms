2b944b097cab04abc43bbb9fff6791b5bf3c6d73
MalindiJohn <malindi.wambua9789@gmail.com>
2023-01-16 21:00:33 +0300

    added hIndex js function logic

--- H-Index-II/H-Index-II.js
+++ H-Index-II/H-Index-II.js
@@ -3,5 +3,20 @@
  * @return {number}
  */
 var hIndex = function(citations) {
+
+    let n = citations.length;
+    let left = 0;
+    let right = n - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (citations[mid] === n - mid) {
+            return n - mid;
+        } else if (citations[mid] < n - mid) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return n - left;
     
 };
