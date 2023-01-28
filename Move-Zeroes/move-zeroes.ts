186691d09c91e0cddf1b872c7043f6ad5d94e6d5
MalindiJohn <malindi.wambua9789@gmail.com>
2023-01-28 21:00:33 +0300

    added moveZeroes function logic

--- Move-Zeroes/move-zeroes.ts
+++ Move-Zeroes/move-zeroes.ts
@@ -3,4 +3,18 @@ Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
 
+    let n: number = nums.length;
+    let left: number = 0;
+    let right: number = 0;
+
+    while (right < n) {
+        if (nums[right] !== 0) {
+            let temp: number = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+        }
+        right++;
+    }
+
 };
