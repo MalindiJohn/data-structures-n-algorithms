ac3b01a5962c3fd9c78cef0b8083e556e9d9dd00
MalindiJohn <malindi.wambua9789@gmail.com>
2023-02-04 21:00:33 +0300

    added moveZeroes js function logic

--- Move-Zeroes/move-zeroes.js
+++ Move-Zeroes/move-zeroes.js
@@ -3,5 +3,16 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
+
+    let n = nums.length;
+    let i = 0;
+    let j = 0;
+    while (j < n) {
+        if (nums[j] !== 0) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            i++;
+        }
+        j++;
+    }
     
 };
