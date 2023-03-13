6be50ec1671f2b9583165dc414d4e3c1abb62fdb
MalindiJohn <malindi.wambua9789@gmail.com>
2023-03-13 23:00:33 +0300

    added findDuplicate function logic

--- Find-the-Duplicate-Number/find-duplicate-number.ts
+++ Find-the-Duplicate-Number/find-duplicate-number.ts
@@ -1,3 +1,20 @@
 function findDuplicate(nums: number[]): number {
 
+    let slow = nums[0];
+    let fast = nums[0];
+
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow !== fast);
+
+    slow = nums[0];
+
+    while (slow !== fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+
+    return slow;
+
 };
