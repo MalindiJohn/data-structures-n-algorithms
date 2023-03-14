6004a953605112cfbeb6a4a1e137f8e3f641f906
MalindiJohn <malindi.wambua9789@gmail.com>
2023-03-14 23:00:33 +0300

    testing

--- Find-the-Duplicate-Number/find-duplicate-number.ts
+++ Find-the-Duplicate-Number/find-duplicate-number.ts
@@ -23,3 +23,18 @@ function findDuplicate(nums: number[]): number {
 console.log(findDuplicate([1, 3, 4, 2, 2])); // 2
 console.log(findDuplicate([3, 1, 3, 4, 2])); // 3
 console.log(findDuplicate([1, 1])); // 1
+console.log(findDuplicate([1, 1, 2])); // 1
+console.log(findDuplicate([2, 2, 2, 2, 2])); // 2
+console.log(findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 9])); // 9
+console.log(findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10])); // 10
+console.log(findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11])); // 11
+console.log(findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12])); // 12
+console.log(findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 13])); // 13
+console.log(findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 14])); // 14
+console.log(findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15])); // 15
+console.log(findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 16])); // 16
+console.log(findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 17])); // 17
+console.log(findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 18])); // 18
+console.log(findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 19])); // 19
+console.log(findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 20])); // 20
+console.log(findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 21])); // 21
