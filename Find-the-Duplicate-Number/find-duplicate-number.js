282eb1fb50d0257cec6a8b5ef650b2e76268a0db
MalindiJohn <malindi.wambua9789@gmail.com>
2023-03-17 23:00:33 +0300

    added findDuplicate js function logic

--- Find-the-Duplicate-Number/find-duplicate-number.js
+++ Find-the-Duplicate-Number/find-duplicate-number.js
@@ -3,5 +3,22 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
+
+    let slow = nums[0];
+    let fast = nums[0];
+    
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow !== fast);
+    
+    slow = nums[0];
+    
+    while (slow !== fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    
+    return slow;
     
 };
