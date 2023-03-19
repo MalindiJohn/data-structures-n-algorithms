3a33b03b1e8479863985b0ee4586cdf41109ab30
MalindiJohn <malindi.wambua9789@gmail.com>
2023-03-19 23:00:33 +0300

    testing

--- Find-the-Duplicate-Number/find-duplicate-number.js
+++ Find-the-Duplicate-Number/find-duplicate-number.js
@@ -29,3 +29,24 @@ console.log(findDuplicate(nums)); // 2
 
 nums = [3, 1, 3, 4, 2];
 console.log(findDuplicate(nums)); // 3
+
+nums = [1, 1];
+console.log(findDuplicate(nums)); // 1
+
+nums = [1, 1, 2];
+console.log(findDuplicate(nums)); // 1
+
+nums = [2, 2, 2, 2, 2];
+console.log(findDuplicate(nums)); // 2
+
+nums = [2, 5, 9, 6, 9, 3, 8, 9, 7, 1];
+console.log(findDuplicate(nums)); // 9
+
+nums = [2, 5, 9, 6, 3, 8, 9, 7, 1, 9];
+console.log(findDuplicate(nums)); // 9
+
+nums = [2, 5, 9, 6, 3, 8, 9, 7, 1, 4];
+console.log(findDuplicate(nums)); // 9
+
+nums = [2, 5, 9, 6, 3, 8, 9, 7, 1, 0];
+console.log(findDuplicate(nums)); // 9
