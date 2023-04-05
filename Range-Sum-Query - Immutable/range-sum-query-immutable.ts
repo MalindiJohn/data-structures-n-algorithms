2407bede10014d5ed4b666f2558f54845339df9f
MalindiJohn <malindi.wambua9789@gmail.com>
2023-04-05 19:00:33 +0300

    added NumArray-constructor function

--- Range-Sum-Query - Immutable/range-sum-query-immutable.ts
+++ Range-Sum-Query - Immutable/range-sum-query-immutable.ts
@@ -4,6 +4,11 @@ class NumArray {
 
     constructor(nums: number[]) {
 
+        this.sums = new Array(nums.length + 1).fill(0);
+        for (let i = 0; i < nums.length; i++) {
+            this.sums[i + 1] = this.sums[i] + nums[i];
+        }
+
     }
 
     sumRange(left: number, right: number): number {
