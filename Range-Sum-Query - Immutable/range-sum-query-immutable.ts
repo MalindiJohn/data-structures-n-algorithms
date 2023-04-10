18221138c2e0d8b3367bc553122381ee63135343
MalindiJohn <malindi.wambua9789@gmail.com>
2023-04-10 19:00:33 +0300

    testing

--- Range-Sum-Query - Immutable/range-sum-query-immutable.ts
+++ Range-Sum-Query - Immutable/range-sum-query-immutable.ts
@@ -28,3 +28,18 @@ class NumArray {
 let numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
 console.log(numArray.sumRange(0, 2)); // 1
 console.log(numArray.sumRange(2, 5)); // -1
+console.log(numArray.sumRange(0, 5)); // -3
+console.log(numArray.sumRange(0, 0)); // -2
+console.log(numArray.sumRange(0, 1)); // -2
+console.log(numArray.sumRange(1, 1)); // 0
+console.log(numArray.sumRange(1, 2)); // 3
+console.log(numArray.sumRange(2, 2)); // 3
+console.log(numArray.sumRange(2, 3)); // -2
+console.log(numArray.sumRange(3, 3)); // -5
+console.log(numArray.sumRange(3, 4)); // -3
+console.log(numArray.sumRange(4, 4)); // 2
+console.log(numArray.sumRange(4, 5)); // 1
+console.log(numArray.sumRange(5, 5)); // -1
+console.log(numArray.sumRange(0, 5)); // -3
+console.log(numArray.sumRange(0, 0)); // -2
+console.log(numArray.sumRange(1, 1)); // 0
