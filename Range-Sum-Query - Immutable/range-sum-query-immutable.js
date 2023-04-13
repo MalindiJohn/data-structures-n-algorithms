770134df024da55dbde418441635bef787127438
MalindiJohn <malindi.wambua9789@gmail.com>
2023-04-13 19:00:33 +0300

    added NumArray-sumRange js function logic

--- Range-Sum-Query - Immutable/range-sum-query-immutable.js
+++ Range-Sum-Query - Immutable/range-sum-query-immutable.js
@@ -13,6 +13,14 @@ var NumArray = function(nums) {
  * @return {number}
  */
 NumArray.prototype.sumRange = function(left, right) {
+
+    let sum = 0;
+    
+    for (let i = left; i <= right; i++) {
+        sum += this.nums[i];
+    }
+    
+    return sum;
     
 };
 
