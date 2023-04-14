64f29a0999e0277d171e0aafb3b1b64cdabebde4
MalindiJohn <malindi.wambua9789@gmail.com>
2023-04-14 19:00:33 +0300

    testing NumArray-sumRange js function logic

--- Range-Sum-Query - Immutable/range-sum-query-immutable.js
+++ Range-Sum-Query - Immutable/range-sum-query-immutable.js
@@ -29,3 +29,10 @@ NumArray.prototype.sumRange = function(left, right) {
  * var obj = new NumArray(nums)
  * var param_1 = obj.sumRange(left,right)
  */
+
+// testing the function
+let nums = [-2, 0, 3, -5, 2, -1];
+let obj = new NumArray(nums);
+console.log(obj.sumRange(0, 2)); // 1
+console.log(obj.sumRange(2, 5)); // -1
+console.log(obj.sumRange(0, 5)); // -3
