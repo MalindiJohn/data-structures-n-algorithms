41cb446a8a7f768b7f8c74834f5e33ffb599eae3
MalindiJohn <malindi.wambua9789@gmail.com>
2023-05-07 19:00:33 +0300

    added NumArray class - buildTree fun logic

--- Range-Sum-Query-Mutable/range-sum-query-mutable.ts
+++ Range-Sum-Query-Mutable/range-sum-query-mutable.ts
@@ -13,6 +13,19 @@ class NumArray {
 
     }
 
+    private buildTree(nums: number[], start: number, end: number, index: number): void {
+        if (start === end) {
+            this.segmentTree[index] = nums[start];
+            return;
+        }
+        let mid = Math.floor((start + end) / 2);
+        // Recursively build the left and right children.
+        this.buildTree(nums, start, mid, 2 * index + 1);
+        this.buildTree(nums, mid + 1, end, 2 * index + 2);
+        // The current node's value is the sum of its children.
+        this.segmentTree[index] = this.segmentTree[2 * index + 1] + this.segmentTree[2 * index + 2];
+    }
+
     update(index: number, val: number): void {
 
     }
