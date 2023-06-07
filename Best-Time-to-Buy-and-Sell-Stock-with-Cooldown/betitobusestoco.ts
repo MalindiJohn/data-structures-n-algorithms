328fdd5ef9ae0c64ad706137181d3308d4dd7b85
MalindiJohn <malindi.wambua9789@gmail.com>
2023-06-07 19:00:33 +0300

    fixed bug of undefined

--- Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/betitobusestoco.ts
+++ Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/betitobusestoco.ts
@@ -1,11 +1,14 @@
 function maxProfit(prices: number[]): number {
 
     let n = prices.length;
+    if (n < 2) return 0; // Add this line to handle cases where prices.length is less than 2
+
     let dp = new Array(n).fill(0).map(() => new Array(2).fill(0));
     dp[0][0] = 0;
     dp[0][1] = -prices[0];
     dp[1][0] = Math.max(0, dp[0][1] + prices[1]);
     dp[1][1] = Math.max(dp[0][1], -prices[1]);
+
     for (let i = 2; i < n; i++) {
         dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
         dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i]);
