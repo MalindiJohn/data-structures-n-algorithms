691430c7d570ba29202c8d370adbc922639d1192
MalindiJohn <malindi.wambua9789@gmail.com>
2023-06-09 19:00:33 +0300

    added maxProfit js function logic

--- Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/betitobusestoco.js
+++ Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/betitobusestoco.js
@@ -3,5 +3,22 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+
+    const n = prices.length;
+    if (n < 2) return 0;
+    
+    const dp = new Array(n).fill(0);
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            let profit = prices[i] - prices[j];
+            if (j >= 2) {
+                profit += dp[j - 2];
+            }
+            dp[i] = Math.max(dp[i], profit);
+        }
+        dp[i] = Math.max(dp[i], dp[i - 1]);
+    }
+    
+    return dp[n - 1];
     
 };
