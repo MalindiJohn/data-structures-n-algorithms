3098683a4bb7c9081813a616801fb3ff3ff4dbc0
MalindiJohn <malindi.wambua9789@gmail.com>
2023-06-13 19:00:33 +0300

    testing

--- Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/betitobusestoco.js
+++ Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/betitobusestoco.js
@@ -27,3 +27,14 @@ var maxProfit = function(prices) {
 console.log(maxProfit([1,2,3,0,2])); // 3
 console.log(maxProfit([1])); // 0
 console.log(maxProfit([1,2])); // 1
+console.log(maxProfit([1,2,3])); // 2
+console.log(maxProfit([1,2,3,4])); // 3
+console.log(maxProfit([1,2,3,4,5])); // 4
+console.log(maxProfit([7,1,5,3,6,4])); // 7
+console.log(maxProfit([7,6,4,3,1])); // 0
+console.log(maxProfit([1,3,2,8,4,9])); // 8
+console.log(maxProfit([3,3,5,0,0,3,1,4])); // 6
+console.log(maxProfit([1,2,4,2,5,7,2,4,9,0])); // 13
+console.log(maxProfit([1,3,2,8,4,9])); // 8
+console.log(maxProfit([1,3,7,5,10,3])); // 9
+console.log(maxProfit([1,2,3,4,5])); // 4
