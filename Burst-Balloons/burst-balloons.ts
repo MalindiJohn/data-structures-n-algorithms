709e3b69266124296202a2b5a1c05ba0477a1290
MalindiJohn <malindi.wambua9789@gmail.com>
2023-06-28 19:00:33 +0300

    added maxCoins function logic

--- Burst-Balloons/burst-balloons.ts
+++ Burst-Balloons/burst-balloons.ts
@@ -1,3 +1,22 @@
 function maxCoins(nums: number[]): number {
 
+    let n = nums.length;
+    if (n === 0) return 0;
+
+    let dp = new Array(n).fill(0).map(() => new Array(n).fill(0));
+
+    for (let len = 1; len <= n; len++) {
+        for (let start = 0; start <= n - len; start++) {
+            let end = start + len - 1;
+            for (let i = start; i <= end; i++) {
+                let coins = nums[i] * getValue(nums, start - 1) * getValue(nums, end + 1);
+                coins += i !== start ? dp[start][i - 1] : 0;
+                coins += i !== end ? dp[i + 1][end] : 0;
+                dp[start][end] = Math.max(dp[start][end], coins);
+            }
+        }
+    }
+
+    return dp[0][n - 1];
+
 };
