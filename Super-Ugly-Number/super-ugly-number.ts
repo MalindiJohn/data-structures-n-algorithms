e004a456a17b1762b4296f191e84e43444ac8cf1
MalindiJohn <malindi.wambua9789@gmail.com>
2023-08-09 19:00:33 +0300

    added nthSuperUglyNumber function logic

--- Super-Ugly-Number/super-ugly-number.ts
+++ Super-Ugly-Number/super-ugly-number.ts
@@ -1,3 +1,22 @@
 function nthSuperUglyNumber(n: number, primes: number[]): number {
 
+    let dp = new Array(n);
+    dp[0] = 1;
+
+    let pointers = new Array(primes.length).fill(0);
+
+    for (let i = 1; i < n; i++) {
+        dp[i] = Number.MAX_SAFE_INTEGER;
+        for (let j = 0; j < primes.length; j++) {
+            dp[i] = Math.min(dp[i], dp[pointers[j]] * primes[j]);
+        }
+        for (let j = 0; j < primes.length; j++) {
+            if (dp[i] === dp[pointers[j]] * primes[j]) {
+                pointers[j]++;
+            }
+        }
+    }
+
+    return dp[n - 1];
+
 };
