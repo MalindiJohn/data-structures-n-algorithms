e1fd2319e655e151e276054624adfe307caf882c
MalindiJohn <malindi.wambua9789@gmail.com>
2023-08-11 19:00:33 +0300

    testing nthSuperUglyNumber function logic

--- Super-Ugly-Number/super-ugly-number.ts
+++ Super-Ugly-Number/super-ugly-number.ts
@@ -20,3 +20,8 @@ function nthSuperUglyNumber(n: number, primes: number[]): number {
     return dp[n - 1];
 
 };
+
+// testing the function
+console.log(nthSuperUglyNumber(12, [2, 7, 13, 19])); // 32
+console.log(nthSuperUglyNumber(1, [2, 3, 5])); // 1
+console.log(nthSuperUglyNumber(2, [2, 3, 5])); // 2
