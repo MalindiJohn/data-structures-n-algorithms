289c3a4d87a8825bcd6d2e9bd8af1581b88ae6b4
MalindiJohn <malindi.wambua9789@gmail.com>
2023-09-05 19:00:33 +0300

    added countSmaller function logic

--- Count-of-Smaller-Numbers-After-Self/count-of-smaller-numbers-after-self.js
+++ Count-of-Smaller-Numbers-After-Self/count-of-smaller-numbers-after-self.js
@@ -3,5 +3,17 @@
  * @return {number[]}
  */
 var countSmaller = function(nums) {
+
+    const n = nums.length;
+    const res = new Array(n).fill(0);
+    const sorted = [];
+    
+    for (let i = n - 1; i >= 0; i--) {
+        const index = binarySearch(sorted, nums[i]);
+        res[i] = index;
+        sorted.splice(index, 0, nums[i]);
+    }
+    
+    return res;
     
 };
