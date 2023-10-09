a4b7620b53ee5f452e77b8bd7f65e9e4732abbc2
MalindiJohn <malindi.wambua9789@gmail.com>
2023-10-09 10:00:33 +0300

    added maxProduct js function logic

--- Maximum-Product-of-Word-Lengths/maximum-product-word-lengths.js
+++ Maximum-Product-of-Word-Lengths/maximum-product-word-lengths.js
@@ -3,5 +3,24 @@
  * @return {number}
  */
 var maxProduct = function(words) {
+
+    let max = 0;
+    let bitMap = new Array(words.length).fill(0);
+
+    for (let i = 0; i < words.length; i++) {
+        for (let j = 0; j < words[i].length; j++) {
+            bitMap[i] |= 1 << words[i].charCodeAt(j) - 97;
+        }
+    }
+
+    for (let i = 0; i < words.length; i++) {
+        for (let j = i + 1; j < words.length; j++) {
+            if ((bitMap[i] & bitMap[j]) === 0) {
+                max = Math.max(max, words[i].length * words[j].length);
+            }
+        }
+    }
+
+    return max;
     
 };
