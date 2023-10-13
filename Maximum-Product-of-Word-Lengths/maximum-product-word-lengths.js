f31d191ac35629ef20305fe5186bddcd31cd12e9
MalindiJohn <malindi.wambua9789@gmail.com>
2023-10-13 10:00:33 +0300

    testing maxProduct js function logic

--- Maximum-Product-of-Word-Lengths/maximum-product-word-lengths.js
+++ Maximum-Product-of-Word-Lengths/maximum-product-word-lengths.js
@@ -31,3 +31,12 @@ console.log(maxProduct(words)); // 16
 
 const words2 = ["a","ab","abc","d","cd","bcd","abcd"];
 console.log(maxProduct(words2)); // 4
+
+const words3 = ["a","aa","aaa","aaaa"];
+console.log(maxProduct(words3)); // 0
+
+const words4 = ["a","ab","abc","abcd","abcde","abcdef","abcdefg","abcdefgh","abcdefghi","abcdefghij","abcdefghijk","abcdefghijkl","abcdefghijklm","abcdefghijklmn","abcdefghijklmno","abcdefghijklmnop","abcdefghijklmnopq","abcdefghijklmnopqr","abcdefghijklmnopqrs","abcdefghijklmnopqrst","abcdefghijklmnopqrstu","abcdefghijklmnopqrstuv","abcdefghijklmnopqrstuvw","abcdefghijklmnopqrstuvwx","abcdefghijklmnopqrstuvwxy","abcdefghijklmnopqrstuvwxyz"];
+console.log(maxProduct(words4)); // 0
+
+const words5 = ["a","ab","abc","abcd","abcde","abcdef","abcdefg","abcdefgh","abcdefghi","abcdefghij","abcdefghijk","abcdefghijkl","abcdefghijklm","abcdefghijklmn","abcdefghijklmno","abcdefghijklmnop","abcdefghijklmnopq","abcdefghijklmnopqr","abcdefghijklmnopqrs","abcdefghijklmnopqrst","abcdefghijklmnopqrstu","abcdefghijklmnopqrstuv","abcdefghijklmnopqrstuvw","abcdefghijklmnopqrstuvwx","abcdefghijklmnopqrstuvwxy","abcdefghijklmnopqrstuvwxyz","zyxwvutsrqponmlkjihgfedcba","yxwvutsrqponmlkjihgfedcba","xwvutsrqponmlkjihgfedcba","wvutsrqponmlkjihgfedcba","vutsrqponmlkjihgfedcba","utsrqponmlkjihgfedcba","tsrqponmlkjihgfedcba","srqponmlkjihgfedcba","rqponmlkjihgfedcba","qponmlkjihgfedcba","ponmlkjihgfedcba","onmlkjihgfedcba","nmlkjihgfedcba","mlkjihgfedcba","lkjihgfedcba","kjihgfedcba","jihgfedcba","ihgfedcba","hgfedcba","gfedcba","fedcba","edcba","dcba","cba","ba","a"];
+console.log(maxProduct(words5)); // 0
