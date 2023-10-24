67827c1b80b1b149b0848343e4ef87bf00182246
MalindiJohn <malindi.wambua9789@gmail.com>
2023-10-24 10:00:33 +0300

    added maxNumber function logic

--- Create-Maximum-Number/create-maximum-number.ts
+++ Create-Maximum-Number/create-maximum-number.ts
@@ -1,3 +1,12 @@
 function maxNumber(nums1: number[], nums2: number[], k: number): number[] {
 
+    let res = [];
+    for (let i = Math.max(0, k - nums2.length); i <= Math.min(k, nums1.length); i++) {
+        let candidate = merge(maxArray(nums1, i), maxArray(nums2, k - i));
+        if (compare(candidate, 0, res, 0) > 0) {
+            res = candidate;
+        }
+    }
+    return res;
+
 };
