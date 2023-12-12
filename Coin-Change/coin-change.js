ec1b7462b694e34f4faa09c56086db57c9893f38
MalindiJohn <malindi.wambua9789@gmail.com>
2023-12-12 10:00:33 +0300

    added coinChange js function logic

--- Coin-Change/coin-change.js
+++ Coin-Change/coin-change.js
@@ -4,5 +4,16 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
+
+    let dp = new Array(amount + 1).fill(Infinity);
+    dp[0] = 0;
+    
+    for (let coin of coins) {
+        for (let i = coin; i <= amount; i++) {
+            dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+        }
+    }
+    
+    return dp[amount] === Infinity ? -1 : dp[amount];
     
 };
