a6dcb16a9694e798a58f40058aa9bc1685371903
MalindiJohn <malindi.wambua9789@gmail.com>
2023-12-15 10:00:33 +0300

    testing coinChange js function logic

--- Coin-Change/coin-change.js
+++ Coin-Change/coin-change.js
@@ -26,3 +26,11 @@ console.log(coinChange(coins, amount)); // 3
 const coins2 = [2];
 const amount2 = 3;
 console.log(coinChange(coins2, amount2)); // -1
+
+const coins3 = [1];
+const amount3 = 0;
+console.log(coinChange(coins3, amount3)); // 0
+
+const coins4 = [1];
+const amount4 = 1;
+console.log(coinChange(coins4, amount4)); // 1
