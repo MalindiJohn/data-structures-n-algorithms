93bccd725085f7066ae04bd3171bb33f942bc961
MalindiJohn <malindi.wambua9789@gmail.com>
2023-12-29 10:00:33 +0300

    added wiggleSort function logic

--- Wiggle-Sort-II/wiggle-sort-II.ts
+++ Wiggle-Sort-II/wiggle-sort-II.ts
@@ -3,4 +3,14 @@
  */
 function wiggleSort(nums: number[]): void {
 
+    const n = nums.length;
+    const sorted = nums.slice().sort((a, b) => a - b);
+    let i = n - 1;
+    for (let j = 1; j < n; j += 2) {
+        nums[j] = sorted[i--];
+    }
+    for (let j = 0; j < n; j += 2) {
+        nums[j] = sorted[i--];
+    }
+
 };
