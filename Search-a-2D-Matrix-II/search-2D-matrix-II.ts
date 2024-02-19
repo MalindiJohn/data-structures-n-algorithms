d142d742ab456634f5836514305bb5e4347ec5d9
MalindiJohn <malindi.wambua9789@gmail.com>
2024-02-19 21:00:33 +0300

    testing searchMatrix function logic

--- Search-a-2D-Matrix-II/search-2D-matrix-II.ts
+++ Search-a-2D-Matrix-II/search-2D-matrix-II.ts
@@ -16,3 +16,36 @@ function searchMatrix(matrix: number[][], target: number): boolean {
     return false;
 
 };
+
+// testing the function
+console.log(searchMatrix([[1, 4, 7, 11, 15],
+[2, 5, 8, 12, 19],
+[3, 6, 9, 16, 22],
+[10, 13, 14, 17, 24],
+[18, 21, 23, 26, 30]], 5)); // expected output : true
+
+console.log(searchMatrix([[1, 4, 7, 11, 15],
+[2, 5, 8, 12, 19],
+[3, 6, 9, 16, 22],
+[10, 13, 14, 17, 24],
+[18, 21, 23, 26, 30]], 20)); // expected output : false
+
+console.log(searchMatrix([[]], 1)); // expected output : false
+
+console.log(searchMatrix([[1]], 1)); // expected output : true
+
+console.log(searchMatrix([[1, 3, 5]], 1)); // expected output : true
+
+console.log(searchMatrix([[1, 3, 5]], 2)); // expected output : false
+
+console.log(searchMatrix([[1], [3], [5]], 1)); // expected output : true
+
+console.log(searchMatrix([[1], [3], [5]], 2)); // expected output : false
+
+console.log(searchMatrix([[1, 4], [2, 5]], 2)); // expected output : true
+
+console.log(searchMatrix([[1, 4], [2, 5]], 3)); // expected output : false
+
+console.log(searchMatrix([[1, 4], [2, 5]], 5)); // expected output : true
+
+console.log(searchMatrix([[1, 4], [2, 5]], 6)); // expected output : false
