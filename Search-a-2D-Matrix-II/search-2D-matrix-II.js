c9263ea87b250706d8f6822a8c9a5af2601b2f2d
MalindiJohn <malindi.wambua9789@gmail.com>
2024-02-21 21:00:33 +0300

    testing searchMatrix js function logic

--- Search-a-2D-Matrix-II/search-2D-matrix-II.js
+++ Search-a-2D-Matrix-II/search-2D-matrix-II.js
@@ -21,5 +21,40 @@ var searchMatrix = function(matrix, target) {
         }
     }
     return false;
-    
+
 };
+
+// testing the function
+let matrix = [
+    [1, 4, 7, 11, 15],
+    [2, 5, 8, 12, 19],
+    [3, 6, 9, 16, 22],
+    [10, 13, 14, 17, 24],
+    [18, 21, 23, 26, 30]
+];
+let target = 5;
+console.log(searchMatrix(matrix, target)); // true
+
+target = 20;
+console.log(searchMatrix(matrix, target)); // false
+
+target = 1;
+console.log(searchMatrix(matrix, target)); // true
+
+target = 30;
+console.log(searchMatrix(matrix, target)); // true
+
+target = 15;
+console.log(searchMatrix(matrix, target)); // true
+
+target = 18;
+console.log(searchMatrix(matrix, target)); // true
+
+target = 26;
+console.log(searchMatrix(matrix, target)); // true
+
+target = 23;
+console.log(searchMatrix(matrix, target)); // true
+
+target = 22;
+console.log(searchMatrix(matrix, target)); // true
