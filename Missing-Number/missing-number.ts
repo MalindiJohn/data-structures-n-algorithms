f0985ceeb7084ebbce9a2b204ed03a4a5a564088
MalindiJohn <malindi.wambua9789@gmail.com>
2024-02-29 21:00:33 +0300

    testing missingNumber function logic

--- Missing-Number/missing-number.ts
+++ Missing-Number/missing-number.ts
@@ -10,3 +10,21 @@ function missingNumber(nums: number[]): number {
     return sum;
 
 };
+
+// testing the function
+console.log(missingNumber([3, 0, 1])); // expected output : 2
+console.log(missingNumber([0, 1])); // expected output : 2
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])); // expected output : 8
+console.log(missingNumber([0])); // expected output : 1
+console.log(missingNumber([1])); // expected output : 0
+console.log(missingNumber([0, 1, 2, 3, 4, 5, 6, 7, 8])); // expected output : 9
+console.log(missingNumber([0, 1, 2, 3, 4, 5, 6, 7, 9])); // expected output : 8
+console.log(missingNumber([0, 1, 2, 3, 4, 5, 6, 8, 9])); // expected output : 7
+console.log(missingNumber([0, 1, 2, 3, 4, 5, 7, 8, 9])); // expected output : 6
+console.log(missingNumber([0, 1, 2, 3, 4, 6, 7, 8, 9])); // expected output : 5
+console.log(missingNumber([0, 1, 2, 3, 5, 6, 7, 8, 9])); // expected output : 4
+console.log(missingNumber([0, 1, 2, 4, 5, 6, 7, 8, 9])); // expected output : 3
+console.log(missingNumber([0, 1, 3, 4, 5, 6, 7, 8, 9])); // expected output : 2
+console.log(missingNumber([0, 2, 3, 4, 5, 6, 7, 8, 9])); // expected output : 1
+console.log(missingNumber([1, 2, 3, 4, 5, 6, 7, 8, 9])); // expected output : 0
+console.log(missingNumber([1, 2, 3, 4, 5, 6, 7, 8, 10])); // expected output : 9
