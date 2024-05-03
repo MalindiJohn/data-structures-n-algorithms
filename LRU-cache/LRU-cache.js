ce9ad66aa302ad87dce422324079d059a120184e
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-03 23:31:11 +0300

    updates

--- LRU-cache/LRU-cache.js
+++ LRU-cache/LRU-cache.js
@@ -7,37 +7,6 @@ var LRUCache = function(capacity) {
     this.cache = new Map();
     this.keys = [];
     
-    this.get = function(key) {
-        if (this.cache.has(key)) {
-            // Remove the key from the keys array
-            let index = this.keys.indexOf(key);
-            this.keys.splice(index, 1);
-            
-            // Add the key to the end of the keys array
-            this.keys.push(key);
-            
-            return this.cache.get(key);
-        }
-        
-        return -1;
-    }
-    
-    this.put = function(key, value) {
-        if (this.cache.has(key)) {
-            // Remove the key from the keys array
-            let index = this.keys.indexOf(key);
-            this.keys.splice(index, 1);
-        } else if (this.keys.length === this.capacity) {
-            // Remove the least recently used key
-            let lruKey = this.keys.shift();
-            this.cache.delete(lruKey);
-        }
-        
-        // Add the key to the end of the keys array
-        this.keys.push(key);
-        this.cache.set(key, value);
-    }
-    
 };
 
 /** 
@@ -88,3 +57,5 @@ LRUCache.prototype.put = function(key, value) {
  * var param_1 = obj.get(key)
  * obj.put(key,value)
  */
+
+console.log()
