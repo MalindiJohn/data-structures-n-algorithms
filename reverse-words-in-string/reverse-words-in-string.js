c7fc5914322ff7ae4f7b9dda0c78bdc66400db21
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-04 23:59:47 +0300

    tested & completed reverse-words-in-string

--- reverse-words-in-string/reverse-words-in-string.js
+++ reverse-words-in-string/reverse-words-in-string.js
@@ -7,3 +7,8 @@ var reverseWords = function(s) {
     return s.split(' ').reverse().join(' ').trim().replace(/\s+/g, ' ');
     
 };
+
+//test the function
+console.log(reverseWords("the sky is blue")); // "blue is sky the"
+console.log(reverseWords("  hello world  ")); // "world hello"
+console.log(reverseWords("a good   example")); // "example good a"
