0cc8c12a6440a46873b16e2bfe0d9b6b303c3ae2
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-05 22:29:25 +0300

    added testing cases

--- numbers-of-islands/numbers-of-islands.js
+++ numbers-of-islands/numbers-of-islands.js
@@ -18,3 +18,36 @@ var numIslands = function(grid) {
     return count;
     
 };
+
+function dfs(grid, i, j) {
+    //write comments in english
+    //if i or j is out of bounds or the current cell is water, return
+    if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] === '0') {
+        return;
+    }
+
+    // mark the current cell as visited
+    grid[i][j] = '0';
+
+    // visit the neighbors of the current cell
+    dfs(grid, i - 1, j);
+    dfs(grid, i + 1, j);
+    dfs(grid, i, j - 1);
+    dfs(grid, i, j + 1);
+}
+
+
+
+// testing the function
+console.log(numIslands([
+    ['1','1','1','1','0'],
+    ['1','1','0','1','0'],
+    ['1','1','0','0','0'],
+    ['0','0','0','0','0']
+])); // 1
+console.log(numIslands([
+    ['1','1','0','0','0'],
+    ['1','1','0','0','0'],
+    ['0','0','1','0','0'],
+    ['0','0','0','1','1']
+])); // 3
