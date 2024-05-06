49331a7c40df75ee6b60fcaf1dbb815edb16d54c
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-06 20:08:14 +0300

    added course schedule logic

--- course-schedule/course-schedule.js
+++ course-schedule/course-schedule.js
@@ -5,6 +5,24 @@
  */
 var canFinish = function(numCourses, prerequisites) {
 
-    
+    let adjList = new Array(numCourses).fill(0).map(() => []);
+
+    for (let i = 0; i < prerequisites.length; i++) {
+        let [course, pre] = prerequisites[i];
+        adjList[pre].push(course);
+    }
+
+    let visited = new Array(numCourses).fill(false);
+    let recStack = new Array(numCourses).fill(false);
+
+    for (let i = 0; i < numCourses; i++) {
+        if (!visited[i]) {
+            if (isCyclic(i, adjList, visited, recStack)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
     
 };
