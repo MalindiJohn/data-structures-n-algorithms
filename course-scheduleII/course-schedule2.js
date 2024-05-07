7c5c0934746df21ee893f586b2d335edad261542
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-07 21:23:38 +0300

    added course schedule logic

--- course-scheduleII/course-schedule2.js
+++ course-scheduleII/course-schedule2.js
@@ -4,5 +4,40 @@
  * @return {number[]}
  */
 var findOrder = function(numCourses, prerequisites) {
+
+    let adjList = new Map();
+    let indegree = new Array(numCourses).fill(0);
+    let queue = [];
+    let result = [];
+    
+    for(let i = 0; i < prerequisites.length; i++) {
+        let [course, pre] = prerequisites[i];
+        if(!adjList.has(pre)) {
+            adjList.set(pre, []);
+        }
+        adjList.get(pre).push(course);
+        indegree[course]++;
+    }
+    
+    for(let i = 0; i < numCourses; i++) {
+        if(indegree[i] === 0) {
+            queue.push(i);
+        }
+    }
+    
+    while(queue.length) {
+        let course = queue.shift();
+        result.push(course);
+        if(adjList.has(course)) {
+            for(let nextCourse of adjList.get(course)) {
+                indegree[nextCourse]--;
+                if(indegree[nextCourse] === 0) {
+                    queue.push(nextCourse);
+                }
+            }
+        }
+    }
+    
+    return result.length === numCourses ? result : [];
     
 };
