360a60108ecbef47fde2e38054edcf6b2ef7fc27
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-07 21:37:16 +0300

    testing & completed the function logic

--- house-robberII/house-robber2.js
+++ house-robberII/house-robber2.js
@@ -15,3 +15,11 @@ var rob = function(nums) {
     return Math.max(robHouses(nums.slice(0, nums.length - 1)), robHouses(nums.slice(1)));
     
 };
+
+// testing the function
+console.log(rob([2,3,2])); // expected 3
+console.log(rob([1,2,3,1])); // expected 4
+console.log(rob([0])); // expected 0
+console.log(rob([1])); // expected 1
+console.log(rob([1,2])); // expected 2
+console.log(rob([1,2,3])); // expected 3
