60e5ae9105c21d6b2b90e27af8cae6befb4e161d
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-07 21:58:47 +0300

    added quickSelect function logic

--- Kth-Largest-Element-in-an-Array/kleiaa.js
+++ Kth-Largest-Element-in-an-Array/kleiaa.js
@@ -10,5 +10,20 @@ var findKthLargest = function(nums, k) {
 };
 
 function quickSelect(nums, left, right, k) {
+
+    if (left === right) {
+        return nums[left];
+    }
+    
+    let pivotIndex = left + Math.floor(Math.random() * (right - left));
+    pivotIndex = partition(nums, left, right, pivotIndex);
+    
+    if (k === pivotIndex + 1) {
+        return nums[pivotIndex];
+    } else if (k < pivotIndex + 1) {
+        return quickSelect(nums, left, pivotIndex - 1, k);
+    } else {
+        return quickSelect(nums, pivotIndex + 1, right, k);
+    }
     
 }
