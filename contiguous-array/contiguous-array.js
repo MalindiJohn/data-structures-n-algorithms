4fe89c816a2d6c58a53e09eb5246e9b434b0aa24
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-08 22:12:16 +0300

    added findMaxLength function logic

--- contiguous-array/contiguous-array.js
+++ contiguous-array/contiguous-array.js
@@ -3,5 +3,22 @@
  * @return {number}
  */
 var findMaxLength = function(nums) {
+
+    let map = new Map();
+    let max = 0;
+    let count = 0;
+    
+    map.set(0, -1);
+    
+    for (let i = 0; i < nums.length; i++) {
+        count += nums[i] === 1 ? 1 : -1;
+        if (map.has(count)) {
+            max = Math.max(max, i - map.get(count));
+        } else {
+            map.set(count, i);
+        }
+    }
+    
+    return max;
     
 };
