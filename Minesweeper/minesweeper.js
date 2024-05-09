67cdc589d63f3855ec79858b4f1d685815d10182
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-09 21:48:01 +0300

    added mineserver functionality logic

--- Minesweeper/minesweeper.js
+++ Minesweeper/minesweeper.js
@@ -4,5 +4,16 @@
  * @return {character[][]}
  */
 var updateBoard = function(board, click) {
+
+    let row = click[0];
+    let col = click[1];
+    
+    if (board[row][col] === 'M') {
+        board[row][col] = 'X';
+    } else {
+        dfs(board, row, col);
+    }
+    
+    return board;
     
 };
