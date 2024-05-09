3c1ec0077e11fc0ab422594457fbd909ad981e6a
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-09 21:23:31 +0300

    added the functionality logic

--- LcswWithadltoetl/LcswWithadltoetl.js
+++ LcswWithadltoetl/LcswWithadltoetl.js
@@ -5,4 +5,34 @@
  */
 var longestSubarray = function(nums, limit) {
     
+    let min = [], max = [];
+    let left = 0, right = 0;
+    let res = 0;
+    
+    while(right < nums.length) {
+        while(min.length > 0 && nums[right] < min[min.length - 1]) {
+            min.pop();
+        }
+        while(max.length > 0 && nums[right] > max[max.length - 1]) {
+            max.pop();
+        }
+        
+        min.push(nums[right]);
+        max.push(nums[right]);
+        
+        while(max[0] - min[0] > limit) {
+            if(nums[left] === min[0]) {
+                min.shift();
+            }
+            if(nums[left] === max[0]) {
+                max.shift();
+            }
+            left++;
+        }
+        
+        res = Math.max(res, right - left + 1);
+        right++;
+    }
+    
+    return res;
 };
