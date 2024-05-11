cfaab45a42944fc4c3c96d2668b45f91ab088ca5
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-11 12:47:49 +0300

    added findMaximumXOR function logic

--- Maximum-XOR-of-Two-Numbers-in-an-Array/mxotniaa.js
+++ Maximum-XOR-of-Two-Numbers-in-an-Array/mxotniaa.js
@@ -3,5 +3,25 @@
  * @return {number}
  */
 var findMaximumXOR = function(nums) {
+
+    let max = 0;
+    let mask = 0;
+    
+    for (let i = 31; i >= 0; i--) {
+        mask = mask | (1 << i);
+        const set = new Set();
+        for (const num of nums) {
+            set.add(num & mask);
+        }
+        const temp = max | (1 << i);
+        for (const prefix of set) {
+            if (set.has(temp ^ prefix)) {
+                max = temp;
+                break;
+            }
+        }
+    }
+    
+    return max;
     
 };
