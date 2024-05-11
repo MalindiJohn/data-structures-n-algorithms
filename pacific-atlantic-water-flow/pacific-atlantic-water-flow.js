9d5cc454e86dc7181a4c1750edda5556eebb9663
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-11 12:39:16 +0300

    testing

--- pacific-atlantic-water-flow/pacific-atlantic-water-flow.js
+++ pacific-atlantic-water-flow/pacific-atlantic-water-flow.js
@@ -52,3 +52,7 @@ var pacificAtlantic = function(heights) {
 // Testing the function 
 const heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]];
 console.log(pacificAtlantic(heights)); // Output: [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
+console.log(pacificAtlantic([[1]]))
+
+// Time Complexity: O(M * N), where M is the number of rows and N is the number of columns in the heights matrix.
+// Space Complexity: O(M * N), where M is the number of rows and N is the number of columns in the heights matrix.
