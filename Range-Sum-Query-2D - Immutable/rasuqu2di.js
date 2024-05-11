50966771eb2ebb1f85bdca94bb0b9fcf93c2a654
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-11 15:26:08 +0300

    testing & complete sumRegion function logic

--- Range-Sum-Query-2D - Immutable/rasuqu2di.js
+++ Range-Sum-Query-2D - Immutable/rasuqu2di.js
@@ -56,3 +56,14 @@ NumMatrix.prototype.sumRegion = function(row1, col1, row2, col2) {
  * var obj = new NumMatrix(matrix)
  * var param_1 = obj.sumRegion(row1,col1,row2,col2)
  */
+
+// Testing the function
+const numMatrix = new NumMatrix([
+    [3, 0, 1, 4, 2],
+    [5, 6, 3, 2, 1],
+    [1, 2, 0, 1, 5],
+    [4, 1, 0, 1, 7],
+    [1, 0, 3, 0, 5]
+]);
+console.log(numMatrix.sumRegion(2, 1, 4, 3)); // Output: 8
+console.log(numMatrix.sumRegion(1, 1, 2, 2)); // Output: 11
