148b57aeec0f65182fa71c28f7e0574b66cc2d77
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-12 23:00:40 +0300

    added fourSumCount function logic

--- 4Sum-II/4Sum-II.js
+++ 4Sum-II/4Sum-II.js
@@ -6,5 +6,24 @@
  * @return {number}
  */
 var fourSumCount = function(nums1, nums2, nums3, nums4) {
+
+    const map = new Map();
+    let count = 0;
+    
+    for (const num1 of nums1) {
+        for (const num2 of nums2) {
+            const sum = num1 + num2;
+            map.set(sum, (map.get(sum) || 0) + 1);
+        }
+    }
+    
+    for (const num3 of nums3) {
+        for (const num4 of nums4) {
+            const sum = num3 + num4;
+            count += map.get(-sum) || 0;
+        }
+    }
+    
+    return count;
     
 };
