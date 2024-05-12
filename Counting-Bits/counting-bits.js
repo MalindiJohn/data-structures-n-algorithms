67268d8ce1ba859521401bb1e7189fd4a9bf8ea3
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-12 23:18:24 +0300

    added countBits function logic

--- Counting-Bits/counting-bits.js
+++ Counting-Bits/counting-bits.js
@@ -3,5 +3,13 @@
  * @return {number[]}
  */
 var countBits = function(n) {
+
+    const dp = new Array(n + 1).fill(0);
+    
+    for (let i = 1; i <= n; i++) {
+        dp[i] = dp[i >> 1] + (i & 1);
+    }
+    
+    return dp;
     
 };
