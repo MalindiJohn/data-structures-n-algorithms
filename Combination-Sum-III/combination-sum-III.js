b0792899088e0acc49fc40c8f34f65695398d820
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-12 23:35:49 +0300

    added combinationSum3 function logic

--- Combination-Sum-III/combination-sum-III.js
+++ Combination-Sum-III/combination-sum-III.js
@@ -4,5 +4,27 @@
  * @return {number[][]}
  */
 var combinationSum3 = function(k, n) {
+
+    const result = [];
+    const combination = [];
+    
+    const backtrack = (start, target) => {
+        if (combination.length === k && target === 0) {
+            result.push([...combination]);
+            return;
+        }
+        for (let i = start; i <= 9; i++) {
+            if (i > target) {
+                break;
+            }
+            combination.push(i);
+            backtrack(i + 1, target - i);
+            combination.pop();
+        }
+    };
+    
+    backtrack(1, n);
+    
+    return result;
     
 };
