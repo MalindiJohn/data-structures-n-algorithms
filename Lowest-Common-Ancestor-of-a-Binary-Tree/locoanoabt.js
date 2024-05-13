7557d6f6e0b9eff4f15cf0b3e2b9df0dd41fce60
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-14 00:28:55 +0300

    added lowestCommonAncestor functionality logic

--- Lowest-Common-Ancestor-of-a-Binary-Tree/locoanoabt.js
+++ Lowest-Common-Ancestor-of-a-Binary-Tree/locoanoabt.js
@@ -12,5 +12,18 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
+
+    if (!root || root === p || root === q) {
+        return root;
+    }
+    
+    const left = lowestCommonAncestor(root.left, p, q);
+    const right = lowestCommonAncestor(root.right, p, q);
+    
+    if (left && right) {
+        return root;
+    }
+    
+    return left || right;
     
 };
