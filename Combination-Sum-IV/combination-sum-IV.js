366cc5714cdc363481393acafc4c680f9f1e45d9
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-14 21:25:39 +0300

    testing & completed combinationSum4 function logic

--- Combination-Sum-IV/combination-sum-IV.js
+++ Combination-Sum-IV/combination-sum-IV.js
@@ -17,3 +17,8 @@ var combinationSum4 = function(nums, target) {
     return dp[target];
     
 };
+
+// Testing the function
+console.log(combinationSum4([1, 2, 3], 4)); // Output 7
+console.log(combinationSum4([9], 3)); // Output 0
+console.log(combinationSum4([1, 2, 3], 32)); // Output 181997601
