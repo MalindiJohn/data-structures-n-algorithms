4cda1c50c4544587b3da9bb326d577fd39c524c0
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-15 23:50:34 +0300

    testing & completed Two-Sum function logic

--- Two-Sum/two-sum.js
+++ Two-Sum/two-sum.js
@@ -13,8 +13,13 @@ var twoSum = function(nums, target) {
         }
         map.set(nums[i], i);
     }
-    
+
     return [];
 
     
 };
+
+// Testing the function
+console.log(twoSum([2, 7, 11, 15], 9)); // Output [0, 1]
+console.log(twoSum([3, 2, 4], 6)); // Output [1, 2]
+console.log(twoSum([3, 3], 6)); // Output [0, 1]
