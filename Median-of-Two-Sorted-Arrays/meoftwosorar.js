e18a360bcf7bb55b1b84530a0122b1268a3b1115
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-16 00:02:53 +0300

    added findMedianSortedArrays function logic

--- Median-of-Two-Sorted-Arrays/meoftwosorar.js
+++ Median-of-Two-Sorted-Arrays/meoftwosorar.js
@@ -4,5 +4,28 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
+
+    let totalLength = nums1.length + nums2.length;
+    let halfLength = Math.floor(totalLength / 2);
+    let i = 0;
+    let j = 0;
+    let current = 0;
+    let previous = 0;
+    while (i + j <= halfLength) {
+        previous = current;
+        if (i < nums1.length && (j >= nums2.length || nums1[i] < nums2[j])) {
+            current = nums1[i];
+            i++;
+        } else {
+            current = nums2[j];
+            j++;
+        }
+    }
+    if (totalLength % 2 === 0) {
+        return (previous + current) / 2;
+    } else {
+        return current;
+    }
+    
     
 };
