592003bc71bbbb5154bb7d66245429e90762411c
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-16 19:56:56 +0300

    added removeElement function logic

--- Remove-Element/remove-element.js
+++ Remove-Element/remove-element.js
@@ -5,4 +5,14 @@
  */
 var removeElement = function(nums, val) {
     
+    let i = 0;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] !== val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    
+    return i;
+    
 };
