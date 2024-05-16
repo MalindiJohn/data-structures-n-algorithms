f34b4ffe7c4fd9a7484409dc752f7780f6f1e79f
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-16 19:21:02 +0300

    added fourSum function logic

--- 4Sum/4Sum.js
+++ 4Sum/4Sum.js
@@ -5,4 +5,38 @@
  */
 var fourSum = function(nums, target) {
     
+    nums.sort((a, b) => a - b);
+    let result = [];
+    
+    for (let i = 0; i < nums.length - 3; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        
+        for (let j = i + 1; j < nums.length - 2; j++) {
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+            
+            let left = j + 1;
+            let right = nums.length - 1;
+            
+            while (left < right) {
+                let sum = nums[i] + nums[j] + nums[left] + nums[right];
+                
+                if (sum === target) {
+                    result.push([nums[i], nums[j], nums[left], nums[right]]);
+                    
+                    while (left < right && nums[left] === nums[left + 1]) left++;
+                    while (left < right && nums[right] === nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+    
+    return result;
+    
 };
