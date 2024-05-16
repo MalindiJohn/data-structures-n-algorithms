89819416c328b36a68065315c7301ff8ec1fb1c2
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-16 19:39:07 +0300

    added removeDuplicates function logic

--- Remove-Duplicates-from-Sorted-Array/redufrosorar.js
+++ Remove-Duplicates-from-Sorted-Array/redufrosorar.js
@@ -4,4 +4,14 @@
  */
 var removeDuplicates = function(nums) {
     
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    
+    return i + 1;
+    
 };
