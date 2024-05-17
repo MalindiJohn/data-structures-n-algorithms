46d2945ba54ed0f816bdb6b26ffaf3f791c2aef9
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-17 22:49:38 +0300

    testing & completed searchInsert function logic

--- Search-Insert-Position/search-insert-position.js
+++ Search-Insert-Position/search-insert-position.js
@@ -12,5 +12,10 @@ var searchInsert = function(nums, target) {
     }
     
     return nums.length;
-    
+
 };
+
+// Testing the function
+console.log(searchInsert([1, 3, 5, 6], 5)); // Output 2
+console.log(searchInsert([1, 3, 5, 6], 2)); // Output 1
+console.log(searchInsert([1, 3, 5, 6], 7)); // Output 4
