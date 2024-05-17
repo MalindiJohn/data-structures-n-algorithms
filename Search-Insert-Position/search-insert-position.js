90a12e150189a5070584d4f24cb9346ae670cffd
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-17 22:51:25 +0300

    testing

--- Search-Insert-Position/search-insert-position.js
+++ Search-Insert-Position/search-insert-position.js
@@ -19,3 +19,27 @@ var searchInsert = function(nums, target) {
 console.log(searchInsert([1, 3, 5, 6], 5)); // Output 2
 console.log(searchInsert([1, 3, 5, 6], 2)); // Output 1
 console.log(searchInsert([1, 3, 5, 6], 7)); // Output 4
+console.log(searchInsert([1, 3, 5, 6], 0)); // Output 0
+console.log(searchInsert([1], 0)); // Output 0
+console.log(searchInsert([1], 1)); // Output 0
+console.log(searchInsert([1], 2)); // Output 1
+console.log(searchInsert([1, 3], 2)); // Output 1
+console.log(searchInsert([1, 3], 4)); // Output 2
+console.log(searchInsert([1, 3], 0)); // Output 0
+console.log(searchInsert([1, 3], 1)); // Output 0
+console.log(searchInsert([1, 3], 3)); // Output 1
+console.log(searchInsert([1, 3], 2)); // Output 1
+console.log(searchInsert([1, 3], 4)); // Output 2
+console.log(searchInsert([1, 3], 0)); // Output 0
+console.log(searchInsert([1, 3], 1)); // Output 0
+console.log(searchInsert([1, 3], 3)); // Output 1
+console.log(searchInsert([1, 3], 2)); // Output 1
+console.log(searchInsert([1, 3], 4)); // Output 2
+console.log(searchInsert([1, 3], 0)); // Output 0
+console.log(searchInsert([1, 3], 1)); // Output 0
+console.log(searchInsert([1, 3], 3)); // Output 1
+console.log(searchInsert([1, 3], 2)); // Output 1
+console.log(searchInsert([1, 3], 4)); // Output 2
+console.log(searchInsert([1, 3], 0)); // Output 0
+console.log(searchInsert([1, 3], 1)); // Output 0
+console.log(searchInsert([1, 3], 3)); // Output 1
