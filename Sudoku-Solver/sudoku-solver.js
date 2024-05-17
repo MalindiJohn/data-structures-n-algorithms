bdc1c9e949acd43c1736f7cda8212b6904bb214d
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-17 23:34:18 +0300

    testing & js completed solveSudoku function logic

--- Sudoku-Solver/sudoku-solver.js
+++ Sudoku-Solver/sudoku-solver.js
@@ -3,28 +3,27 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solveSudoku = function(board) {
-    
-    const isValid = (row, col, num) => {
+
+    function isValid(row, col, num) {
         for (let i = 0; i < 9; i++) {
             if (board[row][i] === num || board[i][col] === num || board[3 * Math.floor(row / 3) + Math.floor(i / 3)][3 * Math.floor(col / 3) + i % 3] === num) {
                 return false;
             }
         }
         return true;
-    };
+    }
     
-    const solve = () => {
+    function solve() {
         for (let i = 0; i < 9; i++) {
             for (let j = 0; j < 9; j++) {
                 if (board[i][j] === '.') {
-                    for (let num = '1'; num <= '9'; num++) {
-                        if (isValid(i, j, num)) {
-                            board[i][j] = num;
+                    for (let num = 1; num <= 9; num++) {
+                        if (isValid(i, j, num.toString())) {
+                            board[i][j] = num.toString();
                             if (solve()) {
                                 return true;
-                            } else {
-                                board[i][j] = '.';
                             }
+                            board[i][j] = '.';
                         }
                     }
                     return false;
@@ -32,7 +31,7 @@ var solveSudoku = function(board) {
             }
         }
         return true;
-    };
+    }
     
     solve();
 };
