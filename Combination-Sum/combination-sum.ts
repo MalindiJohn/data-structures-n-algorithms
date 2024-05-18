c1077a19fd2da4602057b07b1420a4e6704d6e4f
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-18 08:19:45 +0300

    added combinationSum function logic

--- Combination-Sum/combination-sum.ts
+++ Combination-Sum/combination-sum.ts
@@ -1,3 +1,20 @@
 function combinationSum(candidates: number[], target: number): number[][] {
 
+    const result: number[][] = [];
+    const dfs = (target: number, start: number, path: number[]): void => {
+        if (target < 0) return;
+        if (target === 0) {
+            result.push([...path]);
+            return;
+        }
+        for (let i = start; i < candidates.length; i++) {
+            path.push(candidates[i]);
+            dfs(target - candidates[i], i, path);
+            path.pop();
+        }
+    };
+
+    dfs(target, 0, []);
+    return result;
+
 };
