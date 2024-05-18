132598946cd3dfd8865c4a9d34a3b0fd44d91901
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-18 08:25:27 +0300

    added combinationSum js function logic

--- Combination-Sum/combination-sum.js
+++ Combination-Sum/combination-sum.js
@@ -4,5 +4,25 @@
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
+
+    let result = [];
+    
+    function backtrack(start, target, path) {
+        if (target < 0) {
+            return;
+        }
+        if (target === 0) {
+            result.push([...path]);
+            return;
+        }
+        for (let i = start; i < candidates.length; i++) {
+            path.push(candidates[i]);
+            backtrack(i, target - candidates[i], path);
+            path.pop();
+        }
+    }
+    
+    backtrack(0, target, []);
+    return result;
     
 };
