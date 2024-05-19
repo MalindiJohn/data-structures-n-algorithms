4fbb0261a5594ec90b7048dc4debfed5e26e911c
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-19 12:51:26 +0300

    testing trap function logic

--- Trapping-Rain-Water/trapping-rain-water.ts
+++ Trapping-Rain-Water/trapping-rain-water.ts
@@ -27,3 +27,8 @@ function trap(height: number[]): number {
     return result;
 
 };
+
+// testing the function
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])); // expected: 6
+console.log(trap([4, 2, 0, 3, 2, 5])); // expected: 9
+console.log(trap([4, 2, 3])); // expected: 1
