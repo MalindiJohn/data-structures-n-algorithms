c6f513c027b77e920661918c5f683ef99389082f
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-19 12:52:54 +0300

    testing

--- Trapping-Rain-Water/trapping-rain-water.ts
+++ Trapping-Rain-Water/trapping-rain-water.ts
@@ -32,3 +32,17 @@ function trap(height: number[]): number {
 console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])); // expected: 6
 console.log(trap([4, 2, 0, 3, 2, 5])); // expected: 9
 console.log(trap([4, 2, 3])); // expected: 1
+console.log(trap([4, 2, 3, 1])); // expected: 1
+console.log(trap([4, 2, 3, 1, 2])); // expected: 2
+console.log(trap([4, 2, 3, 1, 2, 1])); // expected: 2
+console.log(trap([4, 2, 3, 1, 2, 1, 3])); // expected: 6
+console.log(trap([4, 2, 3, 1, 2, 1, 3, 1])); // expected: 6
+console.log(trap([4, 2, 3, 1, 2, 1, 3, 1, 2])); // expected: 6
+console.log(trap([4, 2, 3, 1, 2, 1, 3, 1, 2, 1])); // expected: 6
+console.log(trap([4, 2, 3, 1, 2, 1, 3, 1, 2, 1, 2])); // expected: 7
+console.log(trap([4, 2, 3, 1, 2, 1, 3, 1, 2, 1, 2, 1])); // expected: 7
+console.log(trap([4, 2, 3, 1, 2, 1, 3, 1, 2, 1, 2, 1, 3])); // expected: 10
+console.log(trap([4, 2, 3, 1, 2, 1, 3, 1, 2, 1, 2, 1, 3, 1])); // expected: 10
+console.log(trap([4, 2, 3, 1, 2, 1, 3, 1, 2, 1, 2, 1, 3, 1, 2])); // expected: 10
+console.log(trap([4, 2, 3, 1, 2, 1, 3, 1, 2, 1, 2, 1, 3, 1, 2, 1])); // expected: 10
+console.log(trap([4, 2, 3, 1, 2, 1, 3, 1, 2, 1, 2, 1, 3, 1, 2, 1, 2])); // expected: 11
