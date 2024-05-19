242732a087560e7b8fe8a17284da07f82a6059e6
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-19 11:42:39 +0300

    added firstMissingPositive function logic

--- First-Missing-Positive/first-missing-positive.ts
+++ First-Missing-Positive/first-missing-positive.ts
@@ -1,3 +1,15 @@
 function firstMissingPositive(nums: number[]): number {
 
+    for (let i = 0; i < nums.length; i++) {
+        while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] !== nums[i]) {
+            [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+        }
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== i + 1) return i + 1;
+    }
+
+    return nums.length + 1;
+
 };
