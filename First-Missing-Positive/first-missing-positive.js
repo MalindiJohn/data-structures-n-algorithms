75c6e3727b02030834822086fedb46494839cfbc
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-19 12:22:55 +0300

    added firstMissingPositive function logic

--- First-Missing-Positive/first-missing-positive.js
+++ First-Missing-Positive/first-missing-positive.js
@@ -3,5 +3,22 @@
  * @return {number}
  */
 var firstMissingPositive = function(nums) {
+
+    let n = nums.length;
+    for (let i = 0; i < n; i++) {
+        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+            let temp = nums[nums[i] - 1];
+            nums[nums[i] - 1] = nums[i];
+            nums[i] = temp;
+        }
+    }
+    
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i + 1) {
+            return i + 1;
+        }
+    }
+    
+    return n + 1;
     
 };
