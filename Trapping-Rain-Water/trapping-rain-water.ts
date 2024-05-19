9a7201cabfe903081b67f8933268896edb87c5bd
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-19 12:50:23 +0300

    added trap function logic

--- Trapping-Rain-Water/trapping-rain-water.ts
+++ Trapping-Rain-Water/trapping-rain-water.ts
@@ -1,3 +1,29 @@
 function trap(height: number[]): number {
 
+    let left = 0;
+    let right = height.length - 1;
+    let leftMax = 0;
+    let rightMax = 0;
+    let result = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                result += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                result += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+
+    return result;
+
 };
