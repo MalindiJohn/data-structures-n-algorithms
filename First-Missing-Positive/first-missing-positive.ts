aaacd03fc7eb8a2776413c17dcb0416c86d530d6
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-19 11:45:44 +0300

    testing

--- First-Missing-Positive/first-missing-positive.ts
+++ First-Missing-Positive/first-missing-positive.ts
@@ -18,3 +18,17 @@ function firstMissingPositive(nums: number[]): number {
 console.log(firstMissingPositive([1, 2, 0])); // expected: 3
 console.log(firstMissingPositive([3, 4, -1, 1])); // expected: 2
 console.log(firstMissingPositive([7, 8, 9, 11, 12])); // expected: 1
+console.log(firstMissingPositive([1])); // expected: 2
+console.log(firstMissingPositive([1, 1])); // expected: 2
+console.log(firstMissingPositive([2, 2])); // expected: 1
+console.log(firstMissingPositive([1, 2, 3])); // expected: 4
+console.log(firstMissingPositive([1, 2, 3, 4])); // expected: 5
+console.log(firstMissingPositive([2, 1, 3, 4])); // expected: 5
+console.log(firstMissingPositive([2, 1, 3, 4, 5])); // expected: 6
+console.log(firstMissingPositive([3, 4, -1, 1, 5])); // expected: 2
+console.log(firstMissingPositive([3, 4, -1, 1, 5, 6])); // expected: 2
+console.log(firstMissingPositive([3, 4, -1, 1, 5, 6, 7])); // expected: 2
+console.log(firstMissingPositive([3, 4, -1, 1, 5, 6, 7, 8])); // expected: 2
+console.log(firstMissingPositive([3, 4, -1, 1, 5, 6, 7, 8, 9])); // expected: 2
+console.log(firstMissingPositive([3, 4, -1, 1, 5, 6, 7, 8, 9, 10])); // expected: 2
+console.log(firstMissingPositive([3, 4, -1, 1, 5, 6, 7, 8, 9, 10, 11])); // expected: 2
