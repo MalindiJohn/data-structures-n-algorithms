37047da6fbb147a9a698ec51b5f78963d0d0b35a
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-20 23:24:23 +0300

    added insert js function logic

--- Insert-Interval/insert-interval.js
+++ Insert-Interval/insert-interval.js
@@ -4,5 +4,29 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
+
+    let result = [];
+    let n = intervals.length;
+    let i = 0;
+
+    while (i < n && intervals[i][1] < newInterval[0]) {
+        result.push(intervals[i]);
+        i++;
+    }
+
+    while (i < n && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+        i++;
+    }
+
+    result.push(newInterval);
+
+    while (i < n) {
+        result.push(intervals[i]);
+        i++;
+    }
+
+    return result;
     
 };
