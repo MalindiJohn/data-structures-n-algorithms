1053bdc59039ba0b4a0ea32d8166dc42ebd3e477
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-20 23:07:29 +0300

    testing

--- Jump-Game/jump-game.js
+++ Jump-Game/jump-game.js
@@ -22,3 +22,17 @@ var canJump = function(nums) {
 console.log(canJump([2, 3, 1, 1, 4])); // Output true
 console.log(canJump([3, 2, 1, 0, 4])); // Output false
 console.log(canJump([0])); // Output true
+console.log(canJump([1])); // Output true
+console.log(canJump([0, 1])); // Output false
+console.log(canJump([1, 0])); // Output true
+console.log(canJump([0, 0])); // Output false
+console.log(canJump([0, 1, 0])); // Output false
+console.log(canJump([0, 2, 0])); // Output false
+console.log(canJump([0, 3, 0])); // Output false
+console.log(canJump([0, 4, 0])); // Output false
+console.log(canJump([0, 5, 0])); // Output false
+console.log(canJump([0, 6, 0])); // Output false
+console.log(canJump([0, 7, 0])); // Output false
+console.log(canJump([0, 8, 0])); // Output false
+console.log(canJump([0, 9, 0])); // Output false
+console.log(canJump([0, 10, 0])); // Output false
