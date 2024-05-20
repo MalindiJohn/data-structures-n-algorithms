fe9ac225bf8b3228f4904f8a0a9105f319c2b5a5
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-20 22:06:48 +0300

    added jum function logic

--- Jump-Game-II/jump-game-II.ts
+++ Jump-Game-II/jump-game-II.ts
@@ -1,3 +1,17 @@
 function jump(nums: number[]): number {
 
+    let jumps = 0;
+    let currentJumpEnd = 0;
+    let farthest = 0;
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        farthest = Math.max(farthest, i + nums[i]);
+        if (i === currentJumpEnd) {
+            jumps++;
+            currentJumpEnd = farthest;
+        }
+    }
+
+    return jumps;
+
 };
