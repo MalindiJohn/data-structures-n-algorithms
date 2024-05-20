43c7515458a67a5447783a03f8c193120a030248
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-20 22:47:01 +0300

    testing spiralOrder js function logic

--- Spiral-Matrix/spiral-matrix.js
+++ Spiral-Matrix/spiral-matrix.js
@@ -38,3 +38,10 @@ var spiralOrder = function(matrix) {
     return result;
     
 };
+
+// Testing the function
+console.log(spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
+// Output [1,2,3,6,9,8,7,4,5]
+console.log(spiralOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]));
+// Output [1,2,3,4,8,12,11,10,9,5,6,7]
+console.log(spiralOrder([[1, 2, 3, 4], [5, 6, 7, 8]]));
