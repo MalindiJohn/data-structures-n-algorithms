4f60b6a4672b6f16c441a3cf2153a2384ac01558
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-20 23:20:36 +0300

    testing insert function logic

--- Insert-Interval/insert-interval.ts
+++ Insert-Interval/insert-interval.ts
@@ -24,3 +24,8 @@ function insert(intervals: number[][], newInterval: number[]): number[][] {
     return result;
 
 };
+
+// testing the function
+console.log(insert([[1, 3], [6, 9]], [2, 5])); // expected [[1,5],[6,9]]
+console.log(insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8])); // expected [[1,2],[3,10],[12,16]]
+console.log(insert([], [5, 7])); // expected [[5,7]]
