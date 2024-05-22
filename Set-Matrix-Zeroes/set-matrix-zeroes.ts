e6b0bc7cf268f69d4e118bc590344460f3c8bc8b
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-22 23:44:21 +0300

    testing

--- Set-Matrix-Zeroes/set-matrix-zeroes.ts
+++ Set-Matrix-Zeroes/set-matrix-zeroes.ts
@@ -64,3 +64,47 @@ const matrix2: number[][] = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]];
 setZeroes(matrix2);
 console.log(matrix2);
 // expected output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+const matrix3: number[][] = [[1, 2, 3, 4], [5, 0, 7, 8], [0, 10, 11, 12], [13, 14, 15, 0]];
+setZeroes(matrix3);
+console.log(matrix3);
+
+const matrix4: number[][] = [[1, 2, 3, 4], [5, 0, 7, 8], [0, 10, 11, 12], [13, 14, 15, 16]];
+setZeroes(matrix4);
+console.log(matrix4);
+// expected output: [[0,0,3,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+const matrix5: number[][] = [[1, 2, 3, 4], [5, 0, 7, 8], [0, 10, 11, 12], [13, 14, 15, 16]];
+setZeroes(matrix5);
+console.log(matrix5);
+// expected output: [[0,0,3,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+const matrix6: number[][] = [[1, 2, 3, 4], [5, 0, 7, 8], [0, 10, 11, 12], [13, 14, 15, 16]];
+setZeroes(matrix6);
+console.log(matrix6);
+// expected output: [[0,0,3,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+const matrix7: number[][] = [[1, 2, 3, 4], [5, 0, 7, 8], [0, 10, 11, 12], [13, 14, 15, 16]];
+setZeroes(matrix7);
+console.log(matrix7);
+// expected output: [[0,0,3,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+const matrix8: number[][] = [[1, 2, 3, 4], [5, 0, 7, 8], [0, 10, 11, 12], [13, 14, 15, 16]];
+setZeroes(matrix8);
+console.log(matrix8);
+// expected output: [[0,0,3,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+const matrix9: number[][] = [[1, 2, 3, 4], [5, 0, 7, 8], [0, 10, 11, 12], [13, 14, 15, 16]];
+setZeroes(matrix9);
+console.log(matrix9);
+// expected output: [[0,0,3,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+const matrix10: number[][] = [[1, 2, 3, 4], [5, 0, 7, 8], [0, 10, 11, 12], [13, 14, 15, 16]];
+setZeroes(matrix10);
+console.log(matrix10);
+// expected output: [[0,0,3,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+const matrix11: number[][] = [[1, 2, 3, 4], [5, 0, 7, 8], [0, 10, 11, 12], [13, 14, 15, 16]];
+setZeroes(matrix11);
+console.log(matrix11);
+// expected output: [[0,0,3,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
