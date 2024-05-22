a72fce76b6f8fc83b05eda06b350cf612da0e6d8
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-22 20:44:04 +0300

    testing generateMatrix js function logic

--- Spiral-Matrix-II/spiral-matrix-II.js
+++ Spiral-Matrix-II/spiral-matrix-II.js
@@ -33,3 +33,11 @@ var generateMatrix = function(n) {
     return matrix;
     
 };
+
+// Testing the function
+console.log(generateMatrix(3));
+// Output [[1,2,3],[8,9,4],[7,6,5]]
+console.log(generateMatrix(1));
+// Output [[1]]
+console.log(generateMatrix(4));
+// Output [[1,2,3,4],[12,13,14,5],[11,16,15,6],[10,9,8,7]]
