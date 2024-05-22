88fc8f14d7013cc5df216ae83da6b2afd353e033
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-22 20:33:47 +0300

    updated generateMatrix function logic

--- Spiral-Matrix-II/spiral-matrix-II.ts
+++ Spiral-Matrix-II/spiral-matrix-II.ts
@@ -1,3 +1,34 @@
 function generateMatrix(n: number): number[][] {
 
+    const matrix = Array(n).fill(0).map(() => Array(n).fill(0));
+    let rowStart = 0;
+    let rowEnd = n - 1;
+    let colStart = 0;
+    let colEnd = n - 1;
+    let num = 1;
+
+    while (rowStart <= rowEnd && colStart <= colEnd) {
+        for (let i = colStart; i <= colEnd; i++) {
+            matrix[rowStart][i] = num++;
+        }
+        rowStart++;
+
+        for (let i = rowStart; i <= rowEnd; i++) {
+            matrix[i][colEnd] = num++;
+        }
+        colEnd--;
+
+        for (let i = colEnd; i >= colStart; i--) {
+            matrix[rowEnd][i] = num++;
+        }
+        rowEnd--;
+
+        for (let i = rowEnd; i >= rowStart; i--) {
+            matrix[i][colStart] = num++;
+        }
+        colStart++;
+    }
+
+    return matrix;
+
 };
