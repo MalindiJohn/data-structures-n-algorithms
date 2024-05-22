39319af5b2053f43e1cfc58496d97440be4a8b18
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-22 21:18:14 +0300

    testing

--- Unique-Paths-II/unique-paths-II.js
+++ Unique-Paths-II/unique-paths-II.js
@@ -43,3 +43,31 @@ console.log(uniquePathsWithObstacles([[0, 1], [0, 0]]));
 // Output 1
 console.log(uniquePathsWithObstacles([[0, 0], [1, 1], [0, 0]]));
 // Output 0
+console.log(uniquePathsWithObstacles([[0, 0], [0, 0]]));
+// Output 2
+console.log(uniquePathsWithObstacles([[0, 0, 0], [0, 0, 0], [0, 0, 0]]));
+// Output 6
+console.log(uniquePathsWithObstacles([[0, 0, 0], [0, 0, 0], [0, 0, 1]]));
+// Output 0
+console.log(uniquePathsWithObstacles([[0, 0, 0], [0, 0, 0], [0, 1, 0]]));
+// Output 2
+console.log(uniquePathsWithObstacles([[0, 0, 0], [0, 0, 0], [1, 0, 0]]));
+// Output 2
+console.log(uniquePathsWithObstacles([[0, 0, 0], [0, 0, 1], [0, 0, 0]]));
+// Output 1
+console.log(uniquePathsWithObstacles([[0, 0, 0], [0, 1, 0], [0, 0, 0]]));
+// Output 2
+console.log(uniquePathsWithObstacles([[0, 0, 0], [1, 0, 0], [0, 0, 0]]));
+// Output 2
+console.log(uniquePathsWithObstacles([[0, 0, 1], [0, 0, 0], [0, 0, 0]]));
+// Output 3
+console.log(uniquePathsWithObstacles([[0, 1, 0], [0, 0, 0], [0, 0, 0]]));
+// Output 3
+console.log(uniquePathsWithObstacles([[1, 0, 0], [0, 0, 0], [0, 0, 0]]));
+// Output 0
+console.log(uniquePathsWithObstacles([[1, 0, 0], [0, 0, 0], [0, 0, 1]]));
+// Output 0
+console.log(uniquePathsWithObstacles([[1, 0, 0], [0, 0, 0], [0, 1, 0]]));
+// Output 0
+console.log(uniquePathsWithObstacles([[1, 0, 0], [0, 0, 0], [1, 0, 0]]));
+// Output 0
