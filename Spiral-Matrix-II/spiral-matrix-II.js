b340f3fb6d0d0333909b405fa1f784250bb978bd
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-22 20:40:42 +0300

    added generateMatrix js function logic

--- Spiral-Matrix-II/spiral-matrix-II.js
+++ Spiral-Matrix-II/spiral-matrix-II.js
@@ -3,5 +3,33 @@
  * @return {number[][]}
  */
 var generateMatrix = function(n) {
+
+    let matrix = new Array(n).fill(0).map(() => new Array(n).fill(0));
+    let top = 0;
+    let bottom = n - 1;
+    let left = 0;
+    let right = n - 1;
+    let num = 1;
+    
+    while (num <= n * n) {
+        for (let i = left; i <= right; i++) {
+            matrix[top][i] = num++;
+        }
+        top++;
+        for (let i = top; i <= bottom; i++) {
+            matrix[i][right] = num++;
+        }
+        right--;
+        for (let i = right; i >= left; i--) {
+            matrix[bottom][i] = num++;
+        }
+        bottom--;
+        for (let i = bottom; i >= top; i--) {
+            matrix[i][left] = num++;
+        }
+        left++;
+    }
+    
+    return matrix;
     
 };
