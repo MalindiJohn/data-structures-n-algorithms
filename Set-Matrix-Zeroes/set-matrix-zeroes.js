91f02d09f578ea0644f952cffae711bbe2243793
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-22 23:51:13 +0300

    testing

--- Set-Matrix-Zeroes/set-matrix-zeroes.js
+++ Set-Matrix-Zeroes/set-matrix-zeroes.js
@@ -80,3 +80,90 @@ console.log(matrix1);
 //     [0,4,5,0],
 //     [0,3,1,0]
 // ]
+
+matrix2 = [
+    [1, 1, 1],
+    [0, 1, 2]
+];
+setZeroes(matrix2);
+console.log(matrix2);
+// Output [
+//     [0,1,1],
+//     [0,0,0]
+// ]
+
+matrix3 = [
+    [1, 0, 3]
+];
+setZeroes(matrix3);
+console.log(matrix3);
+// Output [
+//     [0,0,0]
+// ]
+
+matrix4 = [
+    [1],
+    [0],
+    [3]
+];
+setZeroes(matrix4);
+console.log(matrix4);
+// Output [
+//     [0],
+//     [0],
+//     [0]
+// ]
+
+matrix5 = [
+    [0]
+];
+setZeroes(matrix5);
+console.log(matrix5);
+// Output [
+//     [0]
+// ]
+
+matrix6 = [
+    [1]
+];
+setZeroes(matrix6);
+console.log(matrix6);
+// Output [
+//     [1]
+// ]
+
+matrix7 = [
+    [1, 1]
+];
+setZeroes(matrix7);
+console.log(matrix7);
+// Output [
+//     [1,1]
+// ]
+
+matrix8 = [
+    [1, 0]
+];
+setZeroes(matrix8);
+console.log(matrix8);
+// Output [
+//     [0,0]
+// ]
+
+matrix9 = [
+    [0, 1]
+];
+setZeroes(matrix9);
+console.log(matrix9);
+// Output [
+//     [0,0]
+// ]
+
+matrix10 = [
+    [0, 0]
+];
+setZeroes(matrix10);
+console.log(matrix10);
+// Output [
+//     [0,0]
+// ]
