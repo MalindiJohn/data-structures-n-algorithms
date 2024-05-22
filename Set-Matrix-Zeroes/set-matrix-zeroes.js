0eb54211bd38c966e9e024f49f1ddecab2afa973
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-22 23:46:39 +0300

    adde setZeroes function logic

--- Set-Matrix-Zeroes/set-matrix-zeroes.js
+++ Set-Matrix-Zeroes/set-matrix-zeroes.js
@@ -3,5 +3,53 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
+
+    let m = matrix.length;
+    let n = matrix[0].length;
+    let firstRowZero = false;
+    let firstColZero = false;
+    
+    for (let i = 0; i < m; i++) {
+        if (matrix[i][0] === 0) {
+            firstColZero = true;
+            break;
+        }
+    }
+    
+    for (let i = 0; i < n; i++) {
+        if (matrix[0][i] === 0) {
+            firstRowZero = true;
+            break;
+        }
+    }
+    
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+    
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    
+    if (firstColZero) {
+        for (let i = 0; i < m; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+    
+    if (firstRowZero) {
+        for (let i = 0; i < n; i++) {
+            matrix[0][i] = 0;
+        }
+    }
     
 };
