0ea464768b0939a58611d5bdbd619092c8558233
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-24 00:34:32 +0300

    testing

--- Word-Search/word-search.ts
+++ Word-Search/word-search.ts
@@ -37,3 +37,36 @@ function exist(board: string[][], word: string): boolean {
 console.log(exist([['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E']], 'ABCCED')); // expected true
 console.log(exist([['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E']], 'SEE')); // expected true
 console.log(exist([['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E']], 'ABCB')); // expected false
+console.log(exist([['A', 'B', 'C', 'E']], 'A')); // expected true
+console.log(exist([['A', 'B', 'C', 'E']], 'B')); // expected true
+console.log(exist([['A', 'B', 'C', 'E']], 'C')); // expected true
+console.log(exist([['A', 'B', 'C', 'E']], 'E')); // expected true
+console.log(exist([['A', 'B', 'C', 'E']], 'F')); // expected false
+console.log(exist([['A'], ['B'], ['C'], ['E']], 'A')); // expected true
+console.log(exist([['A'], ['B'], ['C'], ['E']], 'B')); // expected true
+console.log(exist([['A'], ['B'], ['C'], ['E']], 'C')); // expected true
+console.log(exist([['A'], ['B'], ['C'], ['E']], 'E')); // expected true
+console.log(exist([['A'], ['B'], ['C'], ['E']], 'F')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'AB')); // expected true
+console.log(exist([['A', 'B'], ['C', 'D']], 'AC')); // expected true
+console.log(exist([['A', 'B'], ['C', 'D']], 'AD')); // expected true
+console.log(exist([['A', 'B'], ['C', 'D']], 'BC')); // expected true
+console.log(exist([['A', 'B'], ['C', 'D']], 'BD')); // expected true
+console.log(exist([['A', 'B'], ['C', 'D']], 'CD')); // expected true
+console.log(exist([['A', 'B'], ['C', 'D']], 'BA')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'CA')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'DA')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'CB')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'DB')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'DC')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'ABC')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'ABD')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'ACD')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'BCD')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'ABCD')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'DCBA')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'DCBA')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'DCBA')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'DCBA')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'DCBA')); // expected false
+console.log(exist([['A', 'B'], ['C', 'D']], 'DCBA')); // expected false
