9dd49b4288ba2c3f45ba95247bb8cf6c20c880c3
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-24 00:07:14 +0300

    added subsets js function logic

--- Subsets/Subsets.js
+++ Subsets/Subsets.js
@@ -3,5 +3,21 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
+
+    let result = [];
+    
+    function backtrack(start, path) {
+        result.push([...path]);
+        
+        for (let i = start; i < nums.length; i++) {
+            path.push(nums[i]);
+            backtrack(i + 1, path);
+            path.pop();
+        }
+    }
+    
+    backtrack(0, []);
+    
+    return result;
     
 };
