03a4d9a1d2af8ef73c0d28ad4e3c972a5ea05572
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-24 01:15:38 +0300

    added removeDuplicates js function logic

--- Remove-Duplicates-from-Sorted-Array-II/re-du-fro-sor-ar-II.js
+++ Remove-Duplicates-from-Sorted-Array-II/re-du-fro-sor-ar-II.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+
+    let i = 0;
+    
+    for (let n of nums) {
+        if (i < 2 || n > nums[i - 2]) {
+            nums[i++] = n;
+        }
+    }
+    
+    return i;
     
 };
