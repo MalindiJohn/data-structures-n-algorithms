3d28dde0e201fe5a74e8f01bd891129104cb201f
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-23 23:37:19 +0300

    testing

--- Search-a-2D-Matrix/search-a-2D-matrix.ts
+++ Search-a-2D-Matrix/search-a-2D-matrix.ts
@@ -21,3 +21,31 @@ function searchMatrix(matrix: number[][], target: number): boolean {
 console.log(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3)); // expected true
 console.log(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13)); // expected false
 console.log(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 60)); // expected true
+console.log(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 0)); // expected false
+console.log(searchMatrix([[1]], 0)); // expected false
+console.log(searchMatrix([[1]], 1)); // expected true
+console.log(searchMatrix([[1]], 2)); // expected false
+console.log(searchMatrix([[1], [3]], 3)); // expected true
+console.log(searchMatrix([[1], [3]], 2)); // expected false
+console.log(searchMatrix([[1], [3], [5]], 5)); // expected true
+console.log(searchMatrix([[1], [3], [5]], 4)); // expected false
+console.log(searchMatrix([[1], [3], [5]], 6)); // expected false
+console.log(searchMatrix([[1, 3, 5]], 5)); // expected true
+console.log(searchMatrix([[1, 3, 5]], 4)); // expected false
+console.log(searchMatrix([[1, 3, 5]], 6)); // expected false
+console.log(searchMatrix([[1], [3], [5]], 1)); // expected true
+console.log(searchMatrix([[1], [3], [5]], 3)); // expected true
+console.log(searchMatrix([[1], [3], [5]], 5)); // expected true
+console.log(searchMatrix([[1, 3, 5]], 1)); // expected true
+console.log(searchMatrix([[1, 3, 5]], 3)); // expected true
+console.log(searchMatrix([[1, 3, 5]], 5)); // expected true
+console.log(searchMatrix([[1, 3, 5]], 0)); // expected false
+console.log(searchMatrix([[1, 3, 5]], 2)); // expected false
+console.log(searchMatrix([[1, 3, 5]], 4)); // expected false
+console.log(searchMatrix([[1, 3, 5]], 6)); // expected false
+console.log(searchMatrix([[1, 3, 5, 7, 9]], 1)); // expected true
+console.log(searchMatrix([[1, 3, 5, 7, 9]], 3)); // expected true
+console.log(searchMatrix([[1, 3, 5, 7, 9]], 5)); // expected true
+console.log(searchMatrix([[1, 3, 5, 7, 9]], 7)); // expected true
+console.log(searchMatrix([[1, 3, 5, 7, 9]], 9)); // expected true
+console.log(searchMatrix([[1, 3, 5, 7, 9]], 0)); // expected false
