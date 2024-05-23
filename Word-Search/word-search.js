34800e8e443b97e3c4ac3d5765d18d0a08a5f0a3
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-24 00:39:36 +0300

    added exist function logic

--- Word-Search/word-search.js
+++ Word-Search/word-search.js
@@ -4,5 +4,37 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
+
+    let m = board.length;
+    let n = board[0].length;
+    
+    function backtrack(i, j, k) {
+        if (k === word.length) {
+            return true;
+        }
+        
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] !== word[k]) {
+            return false;
+        }
+        
+        let temp = board[i][j];
+        board[i][j] = '#';
+        
+        let found = backtrack(i + 1, j, k + 1) || backtrack(i - 1, j, k + 1) || backtrack(i, j + 1, k + 1) || backtrack(i, j - 1, k + 1);
+        
+        board[i][j] = temp;
+        
+        return found;
+    }
+    
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (backtrack(i, j, 0)) {
+                return true;
+            }
+        }
+    }
+    
+    return false;
     
 };
