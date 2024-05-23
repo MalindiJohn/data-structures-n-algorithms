51d0a763f77471145ea4c3480e81ac086a11b917
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-24 01:18:04 +0300

    testing

--- Remove-Duplicates-from-Sorted-Array-II/re-du-fro-sor-ar-II.js
+++ Remove-Duplicates-from-Sorted-Array-II/re-du-fro-sor-ar-II.js
@@ -22,3 +22,36 @@ console.log(removeDuplicates(nums)); // Output: 5
 
 nums = [0, 0, 1, 1, 1, 1, 2, 3, 3];
 console.log(removeDuplicates(nums)); // Output: 7
+
+nums = [1, 1, 1, 1, 1, 1, 1, 1, 1];
+console.log(removeDuplicates(nums)); // Output: 2
+
+nums = [1, 1, 1, 1, 1, 1, 1, 1, 2];
+console.log(removeDuplicates(nums)); // Output: 3
+
+nums = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2];
+console.log(removeDuplicates(nums)); // Output: 4
+
+nums = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2];
+console.log(removeDuplicates(nums)); // Output: 5
+
+nums = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2];
+console.log(removeDuplicates(nums)); // Output: 6
+
+nums = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2];
+console.log(removeDuplicates(nums)); // Output: 7
+
+nums = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2];
+console.log(removeDuplicates(nums)); // Output: 8
+
+nums = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2];
+console.log(removeDuplicates(nums)); // Output: 9
+
+nums = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2];
+console.log(removeDuplicates(nums)); // Output: 10
+
+nums = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2];
+console.log(removeDuplicates(nums)); // Output: 11
+
+nums = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
+console.log(removeDuplicates(nums)); // Output: 12
