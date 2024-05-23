ed1f4b1bd78a8fa550d4053b336bc0e411c58cc4
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-23 23:57:03 +0300

    added subsets function logic

--- Subsets/Subsets.ts
+++ Subsets/Subsets.ts
@@ -1,3 +1,14 @@
 function subsets(nums: number[]): number[][] {
 
+    let result: number[][] = [[]];
+
+    for (let i = 0; i < nums.length; i++) {
+        let n = result.length;
+        for (let j = 0; j < n; j++) {
+            result.push([...result[j], nums[i]]);
+        }
+    }
+
+    return result;
+
 };
