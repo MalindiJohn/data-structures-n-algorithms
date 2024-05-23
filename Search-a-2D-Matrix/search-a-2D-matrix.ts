02a592b740f90dc3c1b598fa49c3ee54e5255c7f
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-23 23:35:12 +0300

    testing searchMatrix function logic

--- Search-a-2D-Matrix/search-a-2D-matrix.ts
+++ Search-a-2D-Matrix/search-a-2D-matrix.ts
@@ -16,3 +16,8 @@ function searchMatrix(matrix: number[][], target: number): boolean {
     return false;
 
 };
+
+// testing the function
+console.log(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3)); // expected true
+console.log(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13)); // expected false
+console.log(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 60)); // expected true
