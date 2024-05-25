61b4cc4aef4d05b2f5b47a58fb5a1b49aa6e9c73
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-25 23:17:17 +0300

    added sortedArrayToBST function logic

--- Convert-Sorted-Array-to-Binary-Search-Tree/cosoabisetre.js
+++ Convert-Sorted-Array-to-Binary-Search-Tree/cosoabisetre.js
@@ -11,5 +11,21 @@
  * @return {TreeNode}
  */
 var sortedArrayToBST = function(nums) {
+
+    function helper(left, right) {
+        if (left > right) {
+            return null;
+        }
+
+        let mid = Math.floor(left + (right - left) / 2);
+        let root = new TreeNode(nums[mid]);
+
+        root.left = helper(left, mid - 1);
+        root.right = helper(mid + 1, right);
+
+        return root;
+    }
+
+    return helper(0, nums.length - 1);
     
 };
