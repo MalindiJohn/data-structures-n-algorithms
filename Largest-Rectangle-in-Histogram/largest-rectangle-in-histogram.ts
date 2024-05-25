f6b3a5da646604573a13171086dbfa7a622ad276
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-25 10:15:13 +0300

    added largestRectangleArea function logic

--- Largest-Rectangle-in-Histogram/largest-rectangle-in-histogram.ts
+++ Largest-Rectangle-in-Histogram/largest-rectangle-in-histogram.ts
@@ -1,3 +1,17 @@
 function largestRectangleArea(heights: number[]): number {
 
+    let stack: number[] = [];
+    let maxArea = 0;
+
+    for (let i = 0; i <= heights.length; i++) {
+        while (stack.length > 0 && (i === heights.length || heights[i] < heights[stack[stack.length - 1]])) {
+            let height = heights[stack.pop()!];
+            let width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
+            maxArea = Math.max(maxArea, height * width);
+        }
+        stack.push(i);
+    }
+
+    return maxArea;
+
 };
