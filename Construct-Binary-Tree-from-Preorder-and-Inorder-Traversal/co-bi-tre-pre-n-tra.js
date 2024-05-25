933019ec1cc7c3f66567a0a77fdee016752dbb23
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-25 12:57:19 +0300

    added buildTree js function logic

--- Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/co-bi-tre-pre-n-tra.js
+++ Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/co-bi-tre-pre-n-tra.js
@@ -12,5 +12,27 @@
  * @return {TreeNode}
  */
 var buildTree = function(preorder, inorder) {
+
+    let map = new Map();
+    for (let i = 0; i < inorder.length; i++) {
+        map.set(inorder[i], i);
+    }
+
+    function helper(preStart, inStart, inEnd) {
+        if (preStart > preorder.length - 1 || inStart > inEnd) {
+            return null;
+        }
+
+        let root = new TreeNode(preorder[preStart]);
+        let inIndex = map.get(root.val);
+        let leftSize = inIndex - inStart;
+
+        root.left = helper(preStart + 1, inStart, inIndex - 1);
+        root.right = helper(preStart + leftSize + 1, inIndex + 1, inEnd);
+
+        return root;
+    }
+
+    return helper(0, 0, inorder.length - 1);
     
 };
