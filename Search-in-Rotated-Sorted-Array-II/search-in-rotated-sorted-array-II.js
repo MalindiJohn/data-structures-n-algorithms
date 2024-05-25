106cf8ae848bdc0ce6a57b0208f489e9fd300696
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-25 09:37:28 +0300

    testing search js function logic

--- Search-in-Rotated-Sorted-Array-II/search-in-rotated-sorted-array-II.js
+++ Search-in-Rotated-Sorted-Array-II/search-in-rotated-sorted-array-II.js
@@ -36,3 +36,8 @@ var search = function(nums, target) {
     return false;
     
 };
+
+// testing the function
+console.log(search([2,5,6,0,0,1,2], 0)); // true
+console.log(search([2,5,6,0,0,1,2], 3)); // false
+console.log(search([1,0,1,1,1], 0)); // true
