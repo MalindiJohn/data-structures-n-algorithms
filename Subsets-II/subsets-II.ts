f71b783ce04429c5ca3a28aa4bfe2ea613401009
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-25 11:12:43 +0300

    added subsetsWithDup function logic

--- Subsets-II/subsets-II.ts
+++ Subsets-II/subsets-II.ts
@@ -1,3 +1,25 @@
 function subsetsWithDup(nums: number[]): number[][] {
 
+    nums.sort((a, b) => a - b);
+
+    let result: number[][] = [];
+    let subset: number[] = [];
+
+    function backtrack(start: number) {
+        result.push([...subset]);
+
+        for (let i = start; i < nums.length; i++) {
+            if (i > start && nums[i] === nums[i - 1]) {
+                continue;
+            }
+            subset.push(nums[i]);
+            backtrack(i + 1);
+            subset.pop();
+        }
+    }
+
+    backtrack(0);
+
+    return result;
+
 };
