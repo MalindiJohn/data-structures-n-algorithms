66d0521106e3a2d0ba806e2ca21b21e2dfe91848
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-26 12:24:34 +0300

    added generate js function logic

--- Pascal's-Triangle/pascal's-triangle.js
+++ Pascal's-Triangle/pascal's-triangle.js
@@ -3,5 +3,19 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
+
+    let result = [];
+
+    for (let i = 0; i < numRows; i++) {
+        let row = Array(i + 1).fill(1);
+
+        for (let j = 1; j < row.length - 1; j++) {
+            row[j] = result[i - 1][j - 1] + result[i - 1][j];
+        }
+
+        result.push(row);
+    }
+
+    return result;
     
 };
