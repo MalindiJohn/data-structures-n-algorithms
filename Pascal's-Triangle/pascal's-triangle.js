a4205b719237486484b150ece158c6564d3c0132
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-26 12:26:28 +0300

    testing generate js function logic

--- Pascal's-Triangle/pascal's-triangle.js
+++ Pascal's-Triangle/pascal's-triangle.js
@@ -19,3 +19,18 @@ var generate = function(numRows) {
     return result;
     
 };
+
+// testing the function
+console.log(generate(5));
+// [
+//     [1],
+//     [1,1],
+//     [1,2,1],
+//     [1,3,3,1],
+//     [1,4,6,4,1]
+// ]
+
+console.log(generate(1));
+// [[1]]
+
+console.log(generate(2));
