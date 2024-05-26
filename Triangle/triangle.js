f232ebf61399c7f6142c2eb885f3995e2414fdcc
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-26 13:39:03 +0300

    testing minimumTotal js function logic

--- Triangle/triangle.js
+++ Triangle/triangle.js
@@ -13,3 +13,8 @@ var minimumTotal = function(triangle) {
     return triangle[0][0];
     
 };
+
+// testing the function
+console.log(minimumTotal([[2],[3,4],[6,5,7],[4,1,8,3]])); // 11
+console.log(minimumTotal([[-10]])); // -10
+console.log(minimumTotal([[-1],[2,3],[1,-1,-3]])); // -1
