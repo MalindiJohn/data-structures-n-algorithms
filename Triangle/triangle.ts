1c37413068a42531468ba32f7d9ff16a338cddc8
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-26 13:29:45 +0300

    added minimumTotal function logic

--- Triangle/triangle.ts
+++ Triangle/triangle.ts
@@ -1,3 +1,11 @@
 function minimumTotal(triangle: number[][]): number {
 
+    for (let i = triangle.length - 2; i >= 0; i--) {
+        for (let j = 0; j < triangle[i].length; j++) {
+            triangle[i][j] += Math.min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+        }
+    }
+
+    return triangle[0][0];
+
 };
