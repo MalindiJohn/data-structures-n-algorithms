d066a3ccadc9fa00a42cc3d6b7ca4796a578ffdb
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-26 13:01:07 +0300

    added getRow js function logic

--- Pascal's-Triangle-II/pascal's-triangle-II.js
+++ Pascal's-Triangle-II/pascal's-triangle-II.js
@@ -3,5 +3,16 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
+
+    let result = Array(rowIndex + 1).fill(0);
+    result[0] = 1;
+
+    for (let i = 1; i <= rowIndex; i++) {
+        for (let j = i; j > 0; j--) {
+            result[j] += result[j - 1];
+        }
+    }
+
+    return result;
     
 };
