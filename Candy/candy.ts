8191869501b643258d71ce4549444971781dc62e
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-27 23:38:56 +0300

    added candy function logic

--- Candy/candy.ts
+++ Candy/candy.ts
@@ -1,3 +1,19 @@
 function candy(ratings: number[]): number {
 
+    let candies = Array(ratings.length).fill(1);
+
+    for (let i = 1; i < ratings.length; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            candies[i] = candies[i - 1] + 1;
+        }
+    }
+
+    for (let i = ratings.length - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        }
+    }
+
+    return candies.reduce((acc, curr) => acc + curr, 0);
+
 };
