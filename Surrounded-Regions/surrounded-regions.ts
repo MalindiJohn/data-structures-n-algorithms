346c6f392bf7707e03ac6e82ffdf221cff22c0de
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-27 21:40:19 +0300

    added solve function logic

--- Surrounded-Regions/surrounded-regions.ts
+++ Surrounded-Regions/surrounded-regions.ts
@@ -1,6 +1,33 @@
 /**
- Do not return anything, modify board in-place instead.
+Do not return anything, modify board in-place instead.
  */
 function solve(board: string[][]): void {
+
+    const dfs = (i: number, j: number) => {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] !== 'O') {
+            return;
+        }
+        board[i][j] = 'E';
+        dfs(i + 1, j);
+        dfs(i - 1, j);
+        dfs(i, j + 1);
+        dfs(i, j - 1);
+    };
+
+    for (let i = 0; i < board.length; i++) {
+        dfs(i, 0);
+        dfs(i, board[0].length - 1);
+    }
+
+    for (let j = 0; j < board[0].length; j++) {
+        dfs(0, j);
+        dfs(board.length - 1, j);
+    }
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            board[i][j] = board[i][j] === 'E' ? 'O' : 'X';
+        }
+    }
 
 };
