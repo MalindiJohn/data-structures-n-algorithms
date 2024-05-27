9c6a39934b25488a5eedac8e15051aa152f1d33b
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-27 23:44:26 +0300

    added candy js function logic

--- Candy/candy.js
+++ Candy/candy.js
@@ -3,5 +3,21 @@
  * @return {number}
  */
 var candy = function(ratings) {
+
+    let candies = new Array(ratings.length).fill(1);
+
+    for (let i = 1; i < ratings.length; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            candies[i] = candies[i - 1] + 1;
+        }
+    }
+
+    for (let i = ratings.length - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        }
+    }
+
+    return candies.reduce((a, b) => a + b, 0);
     
 };
