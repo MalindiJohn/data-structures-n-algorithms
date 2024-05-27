be2ae61c9516633e346b4d07f3322c37f4462585
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-27 23:41:33 +0300

    testing candy function logic

--- Candy/candy.ts
+++ Candy/candy.ts
@@ -22,3 +22,14 @@ function candy(ratings: number[]): number {
 console.log(candy([1, 0, 2])); // expected 5
 console.log(candy([1, 2, 2])); // expected 4
 console.log(candy([1, 2, 3])); // expected 6
+console.log(candy([3, 2, 1])); // expected 6
+console.log(candy([1, 3, 2])); // expected 5
+console.log(candy([1, 3, 4, 5, 2])); // expected 11
+console.log(candy([1, 2, 3, 4, 5])); // expected 15
+console.log(candy([5, 4, 3, 2, 1])); // expected 15
+console.log(candy([5, 4, 3, 2, 1, 1])); // expected 16
+console.log(candy([5, 4, 3, 2, 1, 1, 1])); // expected 17
+console.log(candy([5, 4, 3, 2, 1, 1, 1, 1])); // expected 18
+console.log(candy([5, 4, 3, 2, 1, 1, 1, 1, 1])); // expected 19
+console.log(candy([5, 4, 3, 2, 1, 1, 1, 1, 1, 1])); // expected 20
+console.log(candy([5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1])); // expected 21
