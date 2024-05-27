3e9ab2960c80edf3e1f4b746a4d9a87ee0b5fef3
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-27 22:33:42 +0300

    added canCompleteCircuit function logic

--- Gas-Station/gas-station.ts
+++ Gas-Station/gas-station.ts
@@ -1,3 +1,21 @@
 function canCompleteCircuit(gas: number[], cost: number[]): number {
 
+    let totalGas = 0;
+    let totalCost = 0;
+    let tank = 0;
+    let start = 0;
+
+    for (let i = 0; i < gas.length; i++) {
+        totalGas += gas[i];
+        totalCost += cost[i];
+        tank += gas[i] - cost[i];
+
+        if (tank < 0) {
+            start = i + 1;
+            tank = 0;
+        }
+    }
+
+    return totalGas < totalCost ? -1 : start;
+
 };
