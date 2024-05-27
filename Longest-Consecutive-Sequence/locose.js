edf0a8b85fff1ef8e31aff213993e72fba4a0211
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-27 21:14:58 +0300

    testing longestConsecutive function logic

--- Longest-Consecutive-Sequence/locose.js
+++ Longest-Consecutive-Sequence/locose.js
@@ -28,3 +28,8 @@ var longestConsecutive = function(nums) {
     return max;
     
 };
+
+// testing the function
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2])); // 4
+console.log(longestConsecutive([0,3,7,2,5,8,4,6,0,1])); // 9
+console.log(longestConsecutive([1,2,0,1])); // 3
