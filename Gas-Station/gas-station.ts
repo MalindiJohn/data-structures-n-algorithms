dba26d3bce865d78e4d9ea6eb78d72c9bae80e20
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-27 22:34:38 +0300

    testing canCompleteCircuit function logic

--- Gas-Station/gas-station.ts
+++ Gas-Station/gas-station.ts
@@ -19,3 +19,8 @@ function canCompleteCircuit(gas: number[], cost: number[]): number {
     return totalGas < totalCost ? -1 : start;
 
 };
+
+// testing the function
+console.log(canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2])); // expected 3
+console.log(canCompleteCircuit([2, 3, 4], [3, 4, 3])); // expected -1
+console.log(canCompleteCircuit([2, 3, 4], [3, 4, 2])); // expected 2
