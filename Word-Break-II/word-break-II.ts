339a316dd993c3bd44e3b86a358cab691d2415fe
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-29 19:29:44 +0300

    added wordBreak function logic

--- Word-Break-II/word-break-II.ts
+++ Word-Break-II/word-break-II.ts
@@ -1,3 +1,7 @@
 function wordBreak(s: string, wordDict: string[]): string[] {
 
+    let wordSet = new Set(wordDict);
+    let memo = new Map<number, string[]>();
+    return dfs(0, s, wordSet, memo);
+
 };
