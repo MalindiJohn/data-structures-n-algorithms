571934e5a0c9acbd23ef306ffae2776191936760
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-29 20:05:51 +0300

    added maxPoints js function logic

--- Max-Points-on-a-Line/max-points-on-a-line.js
+++ Max-Points-on-a-Line/max-points-on-a-line.js
@@ -3,5 +3,35 @@
  * @return {number}
  */
 var maxPoints = function(points) {
+
+    if (points.length < 3) {
+        return points.length;
+    }
+    
+    let max = 0;
+    for (let i = 0; i < points.length; i++) {
+        let map = new Map();
+        let same = 0;
+        let localMax = 0;
+        for (let j = i + 1; j < points.length; j++) {
+            let x = points[j][0] - points[i][0];
+            let y = points[j][1] - points[i][1];
+            if (x === 0 && y === 0) {
+                same++;
+                continue;
+            }
+            let gcd = getGCD(x, y);
+            if (gcd !== 0) {
+                x /= gcd;
+                y /= gcd;
+            }
+            let key = `${x}/${y}`;
+            map.set(key, (map.get(key) || 0) + 1);
+            localMax = Math.max(localMax, map.get(key));
+        }
+        max = Math.max(max, localMax + same + 1);
+    }
+    
+    return max;
     
 };
