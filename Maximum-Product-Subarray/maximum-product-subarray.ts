46154906dfffc0fe1564f89890b81771d2685d8d
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-29 21:16:16 +0300

    added maxProduct function logic

--- Maximum-Product-Subarray/maximum-product-subarray.ts
+++ Maximum-Product-Subarray/maximum-product-subarray.ts
@@ -1,3 +1,19 @@
 function maxProduct(nums: number[]): number {
 
+    let max = nums[0];
+    let min = nums[0];
+    let result = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+
+        let tempMax = Math.max(nums[i], max * nums[i], min * nums[i]);
+        min = Math.min(nums[i], max * nums[i], min * nums[i]);
+        max = tempMax;
+
+        result = Math.max(result, max);
+
+    }
+
+    return result;
+
 };
