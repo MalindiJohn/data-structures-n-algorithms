e8e7950e3490953e18135020ecf120f66b419f52
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-29 20:24:33 +0300

    testing

--- Evaluate-Reverse-Polish-Notation/evaluate-reverse-polish-notation.ts
+++ Evaluate-Reverse-Polish-Notation/evaluate-reverse-polish-notation.ts
@@ -27,3 +27,24 @@ function evalRPN(tokens: string[]): number {
 console.log(evalRPN(["2", "1", "+", "3", "*"])); // expected output : 9
 console.log(evalRPN(["4", "13", "5", "/", "+"])); // expected output : 6
 console.log(evalRPN(["10", "6", "9", "3", "/", "-11", "*", "+", "*", "17", "+", "5", "+"])); // expected output : 22
+console.log(evalRPN(["18"])); // expected output : 18
+console.log(evalRPN(["18", "2", "/"])); // expected output : 9
+console.log(evalRPN(["18", "2", "+"])); // expected output : 20
+console.log(evalRPN(["18", "2", "-"])); // expected output : 16
+console.log(evalRPN(["18", "2", "*"])); // expected output : 36
+console.log(evalRPN(["18", "2", "3", "+", "*"])); // expected output : 60
+console.log(evalRPN(["18", "2", "3", "-", "*"])); // expected output : 30
+console.log(evalRPN(["18", "2", "3", "*", "+"])); // expected output : 24
+console.log(evalRPN(["18", "2", "3", "/", "+"])); // expected output : 18
+console.log(evalRPN(["18", "2", "3", "/", "-"])); // expected output : 16
+console.log(evalRPN(["18", "2", "3", "*", "-"])); // expected output : 12
+console.log(evalRPN(["18", "2", "3", "+", "-"])); // expected output : 13
+console.log(evalRPN(["18", "2", "3", "-", "+"])); // expected output : 17
+console.log(evalRPN(["18", "2", "3", "*", "/"])); // expected output : 3
+console.log(evalRPN(["18", "2", "3", "+", "*"])); // expected output : 60
+console.log(evalRPN(["18", "2", "3", "-", "*"])); // expected output : 30
+console.log(evalRPN(["18", "2", "3", "/", "+"])); // expected output : 18
+console.log(evalRPN(["18", "2", "3", "/", "-"])); // expected output : 16
+console.log(evalRPN(["18", "2", "3", "*", "+"])); // expected output : 24
+console.log(evalRPN(["18", "2", "3", "+", "-"])); // expected output : 13
+console.log(evalRPN(["18", "2", "3", "-", "+"])); // expected output : 17
