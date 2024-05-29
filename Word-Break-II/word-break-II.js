50bf1b7ca877ab2c733aec0b8d857457866225c5
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-29 19:42:02 +0300

    added wordBreak js function logic

--- Word-Break-II/word-break-II.js
+++ Word-Break-II/word-break-II.js
@@ -4,5 +4,36 @@
  * @return {string[]}
  */
 var wordBreak = function(s, wordDict) {
+
+    let result = [];
+    let wordSet = new Set(wordDict);
+    let memo = new Map();
+    
+    let dfs = (start) => {
+        if (memo.has(start)) {
+            return memo.get(start);
+        }
+        
+        if (start === s.length) {
+            return [""];
+        }
+        
+        let res = [];
+        for (let end = start + 1; end <= s.length; end++) {
+            let word = s.substring(start, end);
+            if (wordSet.has(word)) {
+                let list = dfs(end);
+                for (let next of list) {
+                    res.push(word + (next ? " " + next : ""));
+                }
+            }
+        }
+        
+        memo.set(start, res);
+        return res;
+    };
+    
+    result = dfs(0);
+    return result;
     
 };
