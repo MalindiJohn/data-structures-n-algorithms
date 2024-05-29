90f3d0226514699153b3e3f82fed9dd7336a42ed
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-29 20:23:10 +0300

    testing evalRPN function logic

--- Evaluate-Reverse-Polish-Notation/evaluate-reverse-polish-notation.ts
+++ Evaluate-Reverse-Polish-Notation/evaluate-reverse-polish-notation.ts
@@ -22,3 +22,8 @@ function evalRPN(tokens: string[]): number {
     return stack[0];
 
 };
+
+// testing the function
+console.log(evalRPN(["2", "1", "+", "3", "*"])); // expected output : 9
+console.log(evalRPN(["4", "13", "5", "/", "+"])); // expected output : 6
+console.log(evalRPN(["10", "6", "9", "3", "/", "-11", "*", "+", "*", "17", "+", "5", "+"])); // expected output : 22
