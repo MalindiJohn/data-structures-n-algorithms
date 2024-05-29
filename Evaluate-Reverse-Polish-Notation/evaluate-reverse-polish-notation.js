d36aef8d8bf3d0bd9984dce0ce47591723deac33
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-29 20:26:50 +0300

    added evalRPN js function logic

--- Evaluate-Reverse-Polish-Notation/evaluate-reverse-polish-notation.js
+++ Evaluate-Reverse-Polish-Notation/evaluate-reverse-polish-notation.js
@@ -3,5 +3,28 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
+
+    let stack = [];
+    let operators = ["+", "-", "*", "/"];
+    
+    for (let token of tokens) {
+        if (operators.includes(token)) {
+            let b = stack.pop();
+            let a = stack.pop();
+            if (token === "+") {
+                stack.push(a + b);
+            } else if (token === "-") {
+                stack.push(a - b);
+            } else if (token === "*") {
+                stack.push(a * b);
+            } else if (token === "/") {
+                stack.push(a / b | 0);
+            }
+        } else {
+            stack.push(parseInt(token));
+        }
+    }
+    
+    return stack.pop();
     
 };
