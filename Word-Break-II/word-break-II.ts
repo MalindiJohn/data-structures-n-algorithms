c562a02f34ba2940d678ca8d0d41342c950bf0ad
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-29 19:31:55 +0300

    added dfs function logic

--- Word-Break-II/word-break-II.ts
+++ Word-Break-II/word-break-II.ts
@@ -8,5 +8,26 @@ function wordBreak(s: string, wordDict: string[]): string[] {
 
 function dfs(start: number, s: string, wordSet: Set<string>, memo: Map<number, string[]>): string[] {
 
+    if (memo.has(start)) {
+        return memo.get(start);
+    }
+
+    let result: string[] = [];
+    if (start === s.length) {
+        result.push('');
+    }
+
+    for (let end = start + 1; end <= s.length; end++) {
+        let word = s.substring(start, end);
+        if (wordSet.has(word)) {
+            let list = dfs(end, s, wordSet, memo);
+            for (let next of list) {
+                result.push(word + (next === '' ? '' : ' ') + next);
+            }
+        }
+    }
+
+    memo.set(start, result);
+    return result;
 
 }
