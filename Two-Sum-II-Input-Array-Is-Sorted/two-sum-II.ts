a292879eb86c93ab7b51629418bff612bc96a558
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-30 23:51:22 +0300

    added twoSum function logic

---
+++ Two-Sum-II-Input-Array-Is-Sorted/two-sum-II.ts
@@ -0,0 +1,20 @@
+function twoSum(numbers: number[], target: number): number[] {
+
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while (left < right) {
+        let sum = numbers[left] + numbers[right];
+
+        if (sum === target) {
+            return [left + 1, right + 1];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return [];
+
+};
