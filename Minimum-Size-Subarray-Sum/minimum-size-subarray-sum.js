cc9d750946050d1d865ace2132eb3b909a4df60f
MalindiJohn <malindi.wambua9789@gmail.com>
2024-05-31 23:00:25 +0300

    testing minSubArrayLen js function logic

--- Minimum-Size-Subarray-Sum/minimum-size-subarray-sum.js
+++ Minimum-Size-Subarray-Sum/minimum-size-subarray-sum.js
@@ -24,3 +24,17 @@ var minSubArrayLen = function(target, nums) {
     return min === Number.MAX_VALUE ? 0 : min;
     
 };
+
+// testing the function
+console.log(minSubArrayLen(7, [2,3,1,2,4,3])); // 2
+console.log(minSubArrayLen(4, [1,4,4])); // 1
+console.log(minSubArrayLen(11, [1,1,1,1,1,1,1,1])); // 0
+console.log(minSubArrayLen(11, [1,2,3,4,5])); // 3
+console.log(minSubArrayLen(15, [1,2,3,4,5])); // 5
+console.log(minSubArrayLen(100, [1,2,3,4,5])); // 0
+console.log(minSubArrayLen(100, [1,2,3,4,50])); // 1
+console.log(minSubArrayLen(100, [1,2,3,4,50,50])); // 1
+console.log(minSubArrayLen(100, [1,2,3,4,50,50,50])); // 1
+console.log(minSubArrayLen(100, [1,2,3,4,50,50,50,50])); // 1
+console.log(minSubArrayLen(100, [1,2,3,4,50,50,50,50,50])); // 1
+console.log(minSubArrayLen(100, [1,2,3,4,50,50,50,50,50,50])); // 1
