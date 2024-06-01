0a9a0e68cad7055f7e91fd33ad93ad86aa711244
MalindiJohn <malindi.wambua9789@gmail.com>
2024-06-01 11:18:08 +0300

    testing containsDuplicate js function logic

--- Contains-Duplicate/contains-duplicate.js
+++ Contains-Duplicate/contains-duplicate.js
@@ -16,3 +16,15 @@ var containsDuplicate = function(nums) {
     return false;
     
 };
+
+// testing the function
+console.log(containsDuplicate([1,2,3,1])); // true
+console.log(containsDuplicate([1,2,3,4])); // false
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2])); // true
+console.log(containsDuplicate([1,2,3,4,5,6,7,8,9,0])); // false
+console.log(containsDuplicate([1,2,3,4,5,6,7,8,9,0,1])); // true
+console.log(containsDuplicate([1,2,3,4,5,6,7,8,9,0,1,2])); // true
+console.log(containsDuplicate([1,2,3,4,5,6,7,8,9,0,1,2,3])); // true
+console.log(containsDuplicate([1,2,3,4,5,6,7,8,9,0,1,2,3,4])); // true
+console.log(containsDuplicate([1,2,3,4,5,6,7,8,9,0,1,2,3,4,5])); // true
+console.log(containsDuplicate([1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6])); // true
