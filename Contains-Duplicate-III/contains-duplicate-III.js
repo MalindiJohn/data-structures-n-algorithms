0175fa53eec7dd94f73ee72b6d8a230caaeef036
MalindiJohn <malindi.wambua9789@gmail.com>
2024-06-03 22:52:49 +0300

    added containsNearbyAlmostDuplicate js function logic

---
+++ Contains-Duplicate-III/contains-duplicate-III.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} indexDiff
+ * @param {number} valueDiff
+ * @return {boolean}
+ */
+var containsNearbyAlmostDuplicate = function(nums, indexDiff, valueDiff) {
+
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let bucket = Math.floor(nums[i] / (valueDiff + 1));
+        if (map.has(bucket) ||
+            (map.has(bucket - 1) && Math.abs(nums[i] - map.get(bucket - 1)) <= valueDiff) ||
+            (map.has(bucket + 1) && Math.abs(nums[i] - map.get(bucket + 1)) <= valueDiff)) {
+            return true;
+        }
+        map.set(bucket, nums[i]);
+        if (i >= indexDiff) {
+            map.delete(Math.floor(nums[i - indexDiff] / (valueDiff + 1));
+        }
+    }
+    return false;
+    
+};
