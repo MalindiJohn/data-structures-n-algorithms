b7f9aeae418bc332cfabb8539ccefb42cfb38350
MalindiJohn <malindi.wambua9789@gmail.com>
2024-06-03 22:30:43 +0300

    testing containsNearbyDuplicate function logic

--- Contains-Duplicate-II/contains-duplicate-II.ts
+++ Contains-Duplicate-II/contains-duplicate-II.ts
@@ -12,3 +12,20 @@ function containsNearbyDuplicate(nums: number[], k: number): boolean {
     return false;
 
 };
+
+// testing the function
+console.log(containsNearbyDuplicate([1, 2, 3, 1], 3)); // expected true
+console.log(containsNearbyDuplicate([1, 0, 1, 1], 1)); // expected true
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2)); // expected false
+console.log(containsNearbyDuplicate([99, 99], 2)); // expected true
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 3)); // expected true
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 0)); // expected false
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 1)); // expected false
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 4)); // expected true
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 5)); // expected true
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 6)); // expected true
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 7)); // expected false
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 8)); // expected false
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 9)); // expected false
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 10)); // expected false
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 11)); // expected false
