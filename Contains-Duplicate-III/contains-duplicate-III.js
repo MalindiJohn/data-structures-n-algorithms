4cf8469bd679118aff2226b99a1251aade4795c2
MalindiJohn <malindi.wambua9789@gmail.com>
2024-06-03 22:55:35 +0300

    testing containsNearbyAlmostDuplicate js function logic

--- Contains-Duplicate-III/contains-duplicate-III.js
+++ Contains-Duplicate-III/contains-duplicate-III.js
@@ -16,9 +16,45 @@ var containsNearbyAlmostDuplicate = function(nums, indexDiff, valueDiff) {
         }
         map.set(bucket, nums[i]);
         if (i >= indexDiff) {
-            map.delete(Math.floor(nums[i - indexDiff] / (valueDiff + 1));
+            map.delete(Math.floor(nums[i - indexDiff] / (valueDiff + 1)));
         }
     }
     return false;
     
 };
+
+// testing the function
+let nums = [1, 2, 3, 1];
+let indexDiff = 3;
+let valueDiff = 0;
+console.log(containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff)); // true
+
+nums = [1, 0, 1, 1];
+indexDiff = 1;
+valueDiff = 2;
+console.log(containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff)); // true
+
+nums = [1, 5, 9, 1, 5, 9];
+indexDiff = 2;
+valueDiff = 3;
+console.log(containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff)); // false
+
+nums = [1, 2, 3, 1, 2, 3];
+indexDiff = 2;
+valueDiff = 0;
+console.log(containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff)); // false
+
+nums = [99, 99];
+indexDiff = 2;
+valueDiff = 2;
+console.log(containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff)); // true
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9];
+indexDiff = 3;
+valueDiff = 3;
+console.log(containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff)); // true
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9];
+indexDiff = 2;
+valueDiff = 3;
+console.log(containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff)); // false
